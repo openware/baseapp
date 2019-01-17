86ba2eef4fbc3d0586a0d3252be65f69da479b78
Valera Petrusenko <villi1511@gmail.com>
2019-01-17 19:29:10 +0200

    Feature: Integration of drone

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'roots': [
-        '<rootDir>/packages'
+        '<rootDir>'
     ],
     'snapshotSerializers': ['enzyme-to-json/serializer'],
     'setupTestFrameworkScriptFile': '<rootDir>/enzymeSetup.ts',
