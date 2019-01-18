5dbf14dfcbf88745be04b044aef3139caf0a7a99
Valera Petrusenko <villi1511@gmail.com>
2019-01-18 13:57:24 +0200

    Fix: Add default env.js, update components version for fix styles issue

---
+++ public/config/env.js
@@ -0,0 +1,9 @@
+window.env = {
+    api: {
+      gatewayUrl: 'http://localhost:9002/api/v2',
+      rangerUrl: 'ws://localhost:9011',
+    },
+    minutesUntilAutoLogout: '5',
+    withCredentials: false,
+};
+  
