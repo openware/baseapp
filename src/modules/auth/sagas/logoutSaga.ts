dfa91910ed574dac37b578cde371c05e513fdb14
ytkachuk <tka4yk1848@gmail.com>
2019-01-31 18:07:41 +0200

    Fix: change require2fa to false when logout

--- src/modules/auth/sagas/logoutSaga.ts
+++ src/modules/auth/sagas/logoutSaga.ts
@@ -1,6 +1,7 @@
 // tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
 import { API, RequestOptions } from '../../../api';
+import { signInRequire2FA } from '../../auth';
 import { userReset } from '../../profile';
 import { logoutError, LogoutFetch } from '../actions';
 
@@ -12,6 +13,7 @@ export function* logoutSaga(action: LogoutFetch) {
     try {
         yield call(API.delete(requestOptions), '/identity/sessions');
         yield put(userReset());
+        yield put(signInRequire2FA({ require2fa: false }));
     } catch (error) {
         yield put(logoutError(error));
     }
