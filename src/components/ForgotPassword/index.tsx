75f8a28b580e244eb425512cbaaf80ac4cb0a26c
Anton Artemev <aartemiev@heliostech.fr>
2019-02-05 14:11:06 +0200

    Fix: insert EmailForm component (#141)

--- src/components/ForgotPassword/index.tsx
+++ src/components/ForgotPassword/index.tsx
@@ -1,8 +1,4 @@
-import {
-  Button,
-  Input,
-  Modal,
-} from '@openware/components';
+import { EmailForm } from '@openware/components';
 import * as React from 'react';
 import {
   connect,
@@ -11,19 +7,12 @@ import {
 } from 'react-redux';
 import { RouterProps } from 'react-router';
 import { withRouter } from 'react-router-dom';
-import { EMAIL_REGEX } from '../../helpers';
 import {
     forgotPassword,
     RootState,
     selectForgotPasswordError,
     selectForgotPasswordSuccess,
 } from '../../modules';
-
-interface ForgotPasswordState {
-    error: boolean;
-    email: string;
-    showModal: boolean;
-}
 
 interface ReduxProps {
     success: boolean;
@@ -39,51 +28,17 @@ interface DispatchProps {
 
 type Props = RouterProps & ReduxProps & DispatchProps;
 
-class ForgotPasswordComponent extends React.Component<Props, ForgotPasswordState> {
-    public state = {
-        error: false,
-        email: '',
-        showModal: false,
-    };
-
-    public componentWillReceiveProps(next: Props) {
-        if (next.success) {
-            this.setState({ showModal: true });
-        }
-    }
-
+class ForgotPasswordComponent extends React.Component<Props> {
     public render() {
-        const { email, error } = this.state;
         const { backendError } = this.props;
         return (
             <div className="pg-forgot-password-screen">
                 <div className="pg-forgot-password-screen__container">
-                    <div className="pg-forgot-password-screen__container-header">
-                        Forgot Password
-                    </div>
-                    <form className="pg-forgot-password-screen__container-body">
-                        <fieldset className="pg-forgot-password-screen__container-body-item">
-                            <legend>
-                                Email
-                            </legend>
-                            <div className="pg-forgot-password-screen__container-body-item__input">
-                                <Input value={email} onChangeValue={this.handleChangeEmail} />
-                            </div>
-                        </fieldset>
-                    </form>
-                    {error && <div className="pg-forgot-password-screen__container-alert">Wrong format of email</div>}
-                    {backendError && <div className="pg-forgot-password-screen__container-alert">{backendError.message}</div>}
-                    <div className="pg-forgot-password-screen__container-footer">
-                        <Button
-                            className="pg-forgot-password-screen__container-footer-button"
-                            label="Send instructions"
-                            onClick={this.handleSendPassword}
-                        />
-                        <Modal
-                            show={this.state.showModal}
-                            header={this.renderModalHeader()}
-                            content={this.renderModalBody()}
-                            footer={this.renderModalFooter()}
+                    <div className="pg-forgot-password___form">
+                        <EmailForm
+                            OnSubmit={this.handleChangeEmail}
+                            title="Resend Confirmation"
+                            errorMessage={(backendError && backendError.message) ? backendError.message : ''}
                         />
                     </div>
                 </div>
@@ -91,63 +46,8 @@ class ForgotPasswordComponent extends React.Component<Props, ForgotPasswordState
         );
     }
 
-    private handleChangeEmail = (value: string) => {
-        this.setState({
-            email: value,
-        });
-    }
-
-    private handleSendPassword = () => {
-        const { email } = this.state;
-
-        if (email.match(EMAIL_REGEX)) {
-            this.setState({
-              error: false,
-            });
-            this.props.forgotPassword({email});
-        } else {
-          this.setState({
-              error: true,
-          });
-        }
-    }
-
-    private renderModalHeader = () => {
-        return (
-            <div className="pg-exchange-modal-submit-header">
-                CHANGE FORGOTTEN PASSWORD
-            </div>
-        );
-    };
-
-    private renderModalBody = () => {
-        return (
-            <div className="pg-exchange-modal-submit-body">
-                <h2>
-                    To change forgotten  password look for an
-                    email in your inbox that provides further
-                    instruction. If you cannot find the email,
-                    please check your spam email
-                </h2>
-            </div>
-        );
-    };
-
-    private renderModalFooter = () => {
-        return (
-            <div className="pg-exchange-modal-submit-footer">
-                <Button
-                    className="pg-exchange-modal-submit-footer__button-inverse"
-                    label="OK"
-                    onClick={this.closeModal}
-                />
-            </div>
-        );
-    };
-
-    private closeModal = () => {
-        this.setState({showModal: false});
-        this.props.history.push('/signin');
+    private handleChangeEmail = (email: string) => {
+        this.props.forgotPassword({email});
     }
 }
 
