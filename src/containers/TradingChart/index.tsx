1260192ec3350ab3ed2260d2a11c1dde936f9e1d
Alexander <oyershov@heliostech.fr>
2019-06-11 16:10:55 +0300

    Feature: add trading chart config (#824)
    * Feature: add trading chart config
    
    * Fix: add chart config in custom folder

--- src/containers/TradingChart/index.tsx
+++ src/containers/TradingChart/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { connect, MapDispatchToPropsFunction, MapStateToProps } from 'react-redux';
+import {
+    connect,
+    MapDispatchToPropsFunction,
+    MapStateToProps,
+} from 'react-redux';
 import {
     IChartingLibraryWidget,
     LanguageCode,
-    ThemeName,
     widget,
 } from '../../charting_library/charting_library.min';
 import { stdTimezoneOffset } from '../../helpers';
@@ -20,8 +23,19 @@ import {
     selectMarkets,
     selectMarketTickers,
 } from '../../modules';
-import { rangerSubscribeKlineMarket, rangerUnsubscribeKlineMarket } from '../../modules/public/ranger';
-import { CurrentKlineSubscription, dataFeedObject, print } from './api';
+import {
+    rangerSubscribeKlineMarket,
+    rangerUnsubscribeKlineMarket,
+} from '../../modules/public/ranger';
+import {
+    CurrentKlineSubscription,
+    dataFeedObject,
+    print,
+} from './api';
+import {
+    widgetOptions,
+    widgetParams,
+} from './config';
 import { getTradingChartTimezone } from './timezones';
 
 interface ReduxProps {
@@ -44,17 +58,6 @@ type Props = ReduxProps & DispatchProps;
 export class TradingChartComponent extends React.PureComponent<Props> {
     public currentKlineSubscription: CurrentKlineSubscription = {};
     public tvWidget: IChartingLibraryWidget | null = null;
-
-    private params = {
-        interval: '15',
-        containerId: 'tv_chart_container',
-        libraryPath: '/charting_library/',
-        clientId: 'tradingview.com',
-        userId: 'public_user_id',
-        fullscreen: false,
-        autosize: true,
-        studiesOverrides: {},
-    };
 
     private datafeed = dataFeedObject(this, this.props.markets);
 
@@ -94,7 +97,7 @@ export class TradingChartComponent extends React.PureComponent<Props> {
                 <div className="cr-table-header__content">
                     {this.props.currentMarket ? this.props.currentMarket.name : ''}
                 </div>
-                <div id={this.params.containerId} className="pg-trading-chart" />
+                <div id={widgetParams.containerId} className="pg-trading-chart" />
             </React.Fragment>
         );
     }
@@ -104,67 +107,20 @@ export class TradingChartComponent extends React.PureComponent<Props> {
         this.datafeed = dataFeedObject(this, markets);
         const currentTimeOffset = new Date().getTimezoneOffset();
         const clockPeriod = currentTimeOffset === stdTimezoneOffset(new Date()) ? 'STD' : 'DST';
-        const timeframe = '1D';
 
         if (this.props.kline.period) {
-            this.params.interval = this.props.kline.period;
+            widgetParams.interval = this.props.kline.period;
         }
-        const widgetOptions = {
-            debug: false,
+        const defaultWidgetOptions = {
             symbol: currentMarket.id,
-            toolbar_bg: '#1E2841',
             datafeed: this.datafeed,
-            interval: this.params.interval,
-            container_id: this.params.containerId,
-            library_path: this.params.libraryPath,
+            interval: widgetParams.interval,
+            container_id: widgetParams.containerId,
             locale: this.props.lang as LanguageCode,
-            disabled_features: ['use_localstorage_for_settings', 'header_symbol_search'],
-            enabled_features: ['show_animated_logo'],
-            client_id: this.params.clientId,
-            user_id: this.params.userId,
-            fullscreen: this.params.fullscreen,
-            autosize: this.params.autosize,
-            studies_overrides: this.params.studiesOverrides,
-            overrides: {
-                ['symbolWatermarkProperties.color']: '#1E2841',
-                ['volumePaneSize']: 'iny',
-                ['mainSeriesProperties.candleStyle.upColor']: '#54B489',
-                ['mainSeriesProperties.candleStyle.downColor']: '#E85E59',
-                ['mainSeriesProperties.candleStyle.borderUpColor']: '#54B489',
-                ['mainSeriesProperties.candleStyle.borderDownColor']: '#E85E59',
-                ['mainSeriesProperties.candleStyle.wickUpColor']: '#54B489',
-                ['mainSeriesProperties.candleStyle.wickDownColor']: '#E85E59',
-                ['paneProperties.background']: '#1E2841',
-                ['paneProperties.vertGridProperties.color']: '#1E2841',
-                ['paneProperties.vertGridProperties.style']: 1,
-                ['paneProperties.horzGridProperties.color']: '#1E2841',
-                ['paneProperties.horzGridProperties.style']: 1,
-                ['paneProperties.crossHairProperties.color']: '#1E2841',
-                ['paneProperties.crossHairProperties.width']: 1,
-                ['paneProperties.crossHairProperties.style']: 1,
-                ['scalesProperties.backgroundColor']: '#1E2841',
-            },
-            loading_screen: {
-                backgroundColor: '#1E2841',
-            },
             timezone: getTradingChartTimezone(currentTimeOffset, clockPeriod),
-            popup_width: '000',
-            enable_publishing: false,
-            withdateranges: false,
-            hide_side_toolbar: false,
-            theme: 'Dark' as ThemeName,
-            custom_css_url: '/css/tradingview.css',
-            allow_symbol_change: false,
-            details: true,
-            hotlist: true,
-            calendar: true,
-            show_popup_button: true,
-            popup_height: '50',
-            height: 610,
-            timeframe: timeframe,
         };
 
-        this.tvWidget = new widget(widgetOptions);
+        this.tvWidget = new widget({...defaultWidgetOptions, ...widgetOptions});
 
         let previousRange = { from: 0, to: 0 };
         if (kline.range.from !== 0 && kline.range.to !== 0) {
---
+++ src/custom/tradingChartConfig.ts
@@ -0,0 +1,2 @@
+export const customWidgetParams = {};
+export const customWidgetOptions = {};
