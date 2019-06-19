c19992f012497816453f74cb080a5127318740c0
fouren <akushniruk@heliostech.fr>
2019-06-19 09:51:56 -0500

    Fix: timezone STD list

--- src/containers/TradingChart/timezones.test.tsx
+++ src/containers/TradingChart/timezones.test.tsx
@@ -5,8 +5,8 @@ describe('getTradingChartTimezone', () => {
         expect(getTradingChartTimezone(60, 'DST')).toEqual('Europe/London');
         expect(getTradingChartTimezone(-480, 'DST')).toEqual('Asia/Shanghai');
         expect(getTradingChartTimezone(-180, 'DST')).toEqual('Europe/Moscow');
-        expect(getTradingChartTimezone(-120, 'STD')).toEqual('Europe/Athens');
-        expect(getTradingChartTimezone('-120', 'STD')).toEqual('Europe/Athens');
+        expect(getTradingChartTimezone(-120, 'STD')).toEqual('Africa/Cairo');
+        expect(getTradingChartTimezone('-120', 'STD')).toEqual('Africa/Cairo');
     });
 
     it('default to UTC', () => {
