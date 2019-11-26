54c067db045f78ef066916f70f47bac9121fcde2
josadcha <josadcha@heliostech.fr>
2019-11-26 19:11:06 +0200

    Fix: reset state of IEO list and current IEO

--- src/plugins/ieo/modules/public/constants.ts
+++ src/plugins/ieo/modules/public/constants.ts
@@ -1,6 +1,7 @@
 export const IEO_FETCH = 'ieo/FETCH';
 export const IEO_DATA = 'ieo/DATA';
 export const IEO_ERROR = 'ieo/ERROR';
+export const IEO_RESET_DATA = 'ieo/RESET_DATA';
 
 export const IEO_ITEM_FETCH = 'ieo/ITEM_FETCH';
 export const IEO_ITEM_DATA = 'ieo/ITEM_DATA';
