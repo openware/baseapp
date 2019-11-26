5210cf61086c040900e16059409f29d6e3f49854
josadcha <josadcha@heliostech.fr>
2019-11-26 19:35:31 +0200

    Fix: display progress bar percentage with precision

--- src/plugins/ieo/components/IEOCard/index.tsx
+++ src/plugins/ieo/components/IEOCard/index.tsx
@@ -215,7 +215,7 @@ class IEOCardComponent extends React.Component<Props, State> {
         const { countdownValue } = this.state;
         const { supply, tokens_ordered } = this.props.ieo;
 
-        const percentage = Math.round((Number(tokens_ordered) * 100) / Number(supply));
+        const percentage = +Decimal.format((+tokens_ordered * 100) / +supply, 2);
 
         const countDownColorClass = classnames('content__ieo-countdown', {
             'content__ieo-countdown--red': countdownValue && Number(countdownValue.split(':').pop()) % 2 === 0,
