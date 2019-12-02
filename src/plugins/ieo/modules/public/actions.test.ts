bf8b4453e509f7939743fd8c679db2be8d1177b7
josadcha <josadcha@heliostech.fr>
2019-12-02 18:14:52 +0200

    Feature: add tests for ieo metadata saga

--- src/plugins/ieo/modules/public/actions.test.ts
+++ src/plugins/ieo/modules/public/actions.test.ts
@@ -1,8 +1,11 @@
 import * as actions from './actions';
 import {
     IEO_DATA,
+    IEO_DATA_METADATA,
     IEO_ERROR,
+    IEO_ERROR_METADATA,
     IEO_FETCH,
+    IEO_FETCH_METADATA,
     IEO_ITEM_DATA,
     IEO_ITEM_ERROR,
     IEO_ITEM_FETCH,
@@ -54,6 +57,15 @@ describe('IEO actions', () => {
         },
     ];
 
+    const fakeMetadata = {
+        twitter: '',
+        website: '',
+        telegram: '',
+        whitepaper: '',
+        precision: '2',
+        icon_url: 'https://cdn1.iconfinder.com/data/icons/flat-world-currency-1/432/Flat_Currency_Bitcoin-512.png',
+    };
+
     const fakeIEOId = 331;
 
     const fakeIEOItem = fakeIEOList[0];
@@ -98,5 +110,25 @@ describe('IEO actions', () => {
     it('should check ieoRangerData action creator', () => {
         const expectedAction = { type: IEO_UPDATE, payload: fakeIEOList[0] };
         expect(actions.ieoUpdate(fakeIEOList[0])).toEqual(expectedAction);
+    });
+
+    it('should check fetchIEOMetadata action creator', () => {
+        const fakePayload =  { id: fakeIEOId, currency_id: fakeIEOItem.currency_id };
+        const expectedAction = {
+            type: IEO_FETCH_METADATA,
+            payload: fakePayload,
+        };
+        expect(actions.ieoFetchMetadata(fakePayload)).toEqual(expectedAction);
+    });
+
+    it('should check ieoDataMetadata action creator', () => {
+        const expectedAction = { type: IEO_DATA_METADATA, payload: { metadata: fakeMetadata, id: fakeIEOId} };
+        expect(actions.ieoDataMetadata({ metadata: fakeMetadata, id: fakeIEOId})).toEqual(expectedAction);
+    });
+
+    it('should check ieoError action creator', () => {
+        const payload = { code: 500, message: ['Server error'] };
+        const expectedAction = { type: IEO_ERROR_METADATA, payload };
+        expect(actions.ieoMetadataError(payload)).toEqual(expectedAction);
     });
 });
