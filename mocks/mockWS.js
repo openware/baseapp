5fc106a95f7960eae48e2b3af29c0c3f601cf6d4
Camille <cmeulien@heliostech.fr>
2019-12-04 23:15:55 +0200

    Feature: Add incremental orderbook support in mockserver (#19)
    Feature: Mockserver reloads on file changes

--- mocks/mockWS.js
+++ mocks/mockWS.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 const http = require('http');
 const mockserver = require('mockserver');
-const markets = require('./markets.js')
-const RangerMock = require('./ranger.js')
+var markets = require('./markets.js');
+var RangerMock = require('./ranger.js');
 const argv = require('yargs').argv;
+const fs = require('fs');
 const portWS = argv.portWS || 9003;
 
 process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
 
-class Mock {
-    constructor(directory, port, verbose = true) {
-        http.createServer(mockserver(directory, verbose)).listen(port, (error) => {
-            if (error) {
-                console.log(`Mock server ${version} unhandled exception`, error);
-                return;
-            }
+var server = new RangerMock(portWS, markets);
 
-            if (verbose) {
-                const url = `http://0.0.0.0:${port}`.green
-                console.log(`Mockserver serving ${directory} on: ${url}`);
-            }
-        })
-    }
-}
-
-new RangerMock(portWS, markets);
+fs.watch('./mocks', (curr, prev) => {
+  console.log(`reloading mock websocket`);
+  server.close();
+  delete require.cache[require.resolve('./ranger.js')]
+  delete require.cache[require.resolve('./markets.js')]
+  RangerMock = require('./ranger.js')
+  markets = require('./markets.js');
+  server = new RangerMock(portWS, markets);
+});
