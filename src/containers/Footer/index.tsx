c2b5fe6c6acf35ffb40b8b6e64961262798f9310
tuangtvt <34855403+tuangtvt@users.noreply.github.com>
2019-12-22 18:09:12 +0700

    Update index.tsx

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -11,8 +11,8 @@ class FooterComponent extends React.Component<RouterProps> {
         return (
             <React.Fragment>
                 <footer className="pg-footer">
-                    <span>Powered by</span>
-                    <a href="https://www.openware.com">openware.com</a>
+                    <span>Developed by</span>
+                    <a href="https://www.blockchain-global.com">BCG</a>
                 </footer>
             </React.Fragment>
         );
