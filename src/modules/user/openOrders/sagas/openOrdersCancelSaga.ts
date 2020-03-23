66ba99b38fd76857a3a1cbb765d73b6a9886546c
oyershov <oyershov@heliostech.fr>
2020-03-23 17:48:34 +0200

    Fix: change cancel order endpoint on finex enabled

--- src/modules/user/openOrders/sagas/openOrdersCancelSaga.ts
+++ src/modules/user/openOrders/sagas/openOrdersCancelSaga.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-import { API, RequestOptions } from '../../../../api';
+import { API, isFinexEnabled, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../index';
 import { openOrdersCancelError, OpenOrdersCancelFetch } from '../actions';
 import { getCsrfToken, getOrderAPI } from '../../../../helpers';
@@ -15,10 +15,18 @@ const ordersCancelOptions = (csrfToken?: string): RequestOptions => {
 export function* openOrdersCancelSaga(action: OpenOrdersCancelFetch) {
     try {
         const { order: { id, uuid } } = action.payload;
-        if (uuid) {
-            yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/${uuid}/cancel`, { uuid });
+        if (isFinexEnabled()) {
+            if (uuid) {
+                yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/cancel/${uuid}`, { uuid });
+            } else {
+                yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/cancel/${id}`, { id });
+            }
         } else {
-            yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/${id}/cancel`, { id });
+            if (uuid) {
+                yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/${uuid}/cancel`, { uuid });
+            } else {
+                yield call(API.post(ordersCancelOptions(getCsrfToken())), `/market/orders/${id}/cancel`, { id });
+            }
         }
 
         yield put(alertPush({ message: ['success.order.cancelling'], type: 'success'}));
