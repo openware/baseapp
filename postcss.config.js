5cd2b3c191ded939834e9c8dacd00d42fcb65300
Oleksandr Khlopiachyi <akhlopiachyi@heliostech.fr>
2020-03-26 17:33:38 +0200

    Feature: add stylelinter config

--- postcss.config.js
+++ postcss.config.js
@@ -5,14 +5,6 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-nested'),
-        require('postcss-autoreset')({
-            reset: {
-                color: 'var(--base)',
-                'font-size': 'var(--font-size)',
-                'font-family': 'var(--font-family)',
-                'background-color': 'var(--background-dark)',
-            },
-        }),
         require('postcss-hexrgba'),
         require('autoprefixer'),
     ],
