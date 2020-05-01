f5c61f3cd80873e1db04a150676d5a7d7d2b7bdb
oyershov <oyershov@heliostech.fr>
2020-05-01 10:48:23 +0300

    Fix: change total value precision

--- src/components/OrderForm/index.tsx
+++ src/components/OrderForm/index.tsx
@@ -248,11 +248,11 @@ export class OrderForm extends React.PureComponent<OrderFormProps, OrderFormStat
                         <div className="cr-order-item__total__content">
                             {orderType === 'Limit' ? (
                                 <span className="cr-order-item__total__content__amount">
-                                    {total.toFixed(currentMarketBidPrecision + currentMarketAskPrecision)}
+                                    {total.toFixed(currentMarketBidPrecision)}
                                 </span>
                             ) : (
                                 <span className="cr-order-item__total__content__amount">
-                                    &asymp;{total.toFixed(currentMarketBidPrecision + currentMarketAskPrecision)}
+                                    &asymp;{total.toFixed(currentMarketBidPrecision)}
                                 </span>
                             )}
                             <span className="cr-order-item__total__content__currency">
