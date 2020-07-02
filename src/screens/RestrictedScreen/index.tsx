366a17db5a498f8d1deb729453bf3f45bd9636b5
Oleksandr Khlopiachyi <akhlopiachyi@heliostech.fr>
2020-07-02 16:56:19 +0300

    Fix: change codestyle and styles

--- src/screens/RestrictedScreen/index.tsx
+++ src/screens/RestrictedScreen/index.tsx
@@ -5,24 +5,24 @@ import { setDocumentTitle } from '../../helpers';
 type Props = InjectedIntlProps;
 
 class Restricted extends React.Component<Props> {
-  public componentDidMount() {
-    setDocumentTitle('404');
-  }
+    public componentDidMount() {
+        setDocumentTitle('404');
+    }
 
-  public render() {
-    return(
-      <div className="pg-restricted-screen">
-        <div className="pg-restricted-screen__title">
-          <h1>404</h1>
-        </div>
-        <div className="pg-restricted-screen__content">
-          <p>{this.translate('page.body.restricted')}</p>
-        </div>
-      </div>
-    );
-  }
+    public render() {
+        return(
+            <div className="pg-restricted-screen">
+                <div className="pg-restricted-screen__title">
+                    404
+                </div>
+                <div className="pg-restricted-screen__content">
+                    {this.translate('page.body.restricted')}
+                </div>
+            </div>
+        );
+    }
 
-  private translate = (key: string) => this.props.intl.formatMessage({id: key});
+    private translate = (key: string) => this.props.intl.formatMessage({id: key});
 }
 
 export const RestrictedScreen = injectIntl(Restricted) as any;
