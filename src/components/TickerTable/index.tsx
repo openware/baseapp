59ab902060d1b16813a7a6630a5cf0593f6c7359
Alexander <oyershov@heliostech.fr>
2020-08-05 12:26:12 +0300

    Feature: refactor markets table with hooks (#212)
    * Feature: refactor markets table using hooks
    * Fix: add default param to useEffect

--- src/components/TickerTable/index.tsx
+++ src/components/TickerTable/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useIntl } from 'react-intl';
 import { Decimal } from '../../components';
 import { Market } from '../../modules';
 
@@ -8,110 +9,101 @@ interface Props {
     markets: Market[];
     redirectToTrading: (key: string) => void;
     setCurrentBidUnit: (key: string) => void;
-    translate: (id: string) => string;
 }
 
-export class TickerTable extends React.Component<Props> {
-    public renderHeader(currentBidUnit: string) {
-        const {
-            currentBidUnitsList,
-            setCurrentBidUnit,
-            translate,
-        } = this.props;
+const renderHeader = (props: Props, intl, currentBidUnit: string) => {
+    const { currentBidUnitsList, setCurrentBidUnit } = props;
 
-        return (
-            <ul className="navigation" role="tablist">
-                {currentBidUnitsList.map((item, i) => (
-                    <li
-                        key={i}
-                        className={`navigation__item ${item === currentBidUnit && 'navigation__item--active'}`}
-                        onClick={() => setCurrentBidUnit(item)}
-                    >
-                        <span className="navigation__item__link">
-                            {item ? item.toUpperCase() : translate('page.body.marketsTable.filter.all')}
-                        </span>
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+    return (
+        <ul className="navigation" role="tablist">
+            {currentBidUnitsList.map((item, i) => (
+                <li
+                    key={i}
+                    className={`navigation__item ${item === currentBidUnit && 'navigation__item--active'}`}
+                    onClick={() => setCurrentBidUnit(item)}
+                >
+                    <span className="navigation__item__link">
+                        {item ? item.toUpperCase() : intl.formatMessage({id: 'page.body.marketsTable.filter.all'})}
+                    </span>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
-    public render() {
-        const {
-            currentBidUnit,
-            markets,
-            translate,
-        } = this.props;
 
-        return (
-            <div className="pg-ticker-table">
-                <div className="pg-ticker-table__filter">
-                    {this.renderHeader(currentBidUnit)}
+const renderItem = (props: Props, market, index: number) => {
+    const { redirectToTrading } = props;
+    const marketChangeColor = +(market.change || 0) < 0 ? 'negative' : 'positive';
+
+    return (
+        <tr key={index} onClick={() => redirectToTrading(market.id)}>
+            <td>
+                <div>
+                    {market && market.name}
                 </div>
-                <div className="pg-ticker-table__table-wrap">
-                    <table className="pg-ticker-table__table">
-                        <thead>
-                            <tr>
-                                <th scope="col">{translate('page.body.marketsTable.header.pair')}</th>
-                                <th scope="col">{translate('page.body.marketsTable.header.lastPrice')}</th>
-                                <th scope="col">{translate('page.body.marketsTable.header.change')}</th>
-                                <th scope="col">{translate('page.body.marketsTable.header.high')}</th>
-                                <th scope="col">{translate('page.body.marketsTable.header.low')}</th>
-                                <th scope="col">{translate('page.body.marketsTable.header.volume')}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {markets[0] && markets.map(this.renderItem)}
-                        </tbody>
-                    </table>
-                </div>
+            </td>
+            <td>
+                <span>
+                    <Decimal fixed={market.amount_precision} thousSep=",">
+                        {market.last}
+                    </Decimal>
+                </span>
+            </td>
+            <td>
+                <span className={marketChangeColor}>{market.price_change_percent}</span>
+            </td>
+            <td>
+                <span>
+                    <Decimal fixed={market.amount_precision} thousSep=",">
+                        {market.high}
+                    </Decimal>
+                </span>
+            </td>
+            <td>
+                <span>
+                    <Decimal fixed={market.amount_precision} thousSep=",">
+                        {market.low}
+                    </Decimal>
+                </span>
+            </td>
+            <td>
+                <span>
+                    <Decimal fixed={market.amount_precision} thousSep=",">
+                        {market.volume}
+                    </Decimal>
+                </span>
+            </td>
+        </tr>
+    );
+};
+
+export const TickerTable = (props: Props) => {
+    const { currentBidUnit, markets } = props;
+    const intl = useIntl();
+
+    return (
+        <div className="pg-ticker-table">
+            <div className="pg-ticker-table__filter">
+                {renderHeader(props, intl, currentBidUnit)}
             </div>
-        );
-    }
-
-    private renderItem = (market, index: number) => {
-        const { redirectToTrading } = this.props;
-        const marketChangeColor = +(market.change || 0) < 0 ? 'negative' : 'positive';
-
-        return (
-            <tr key={index} onClick={() => redirectToTrading(market.id)}>
-                <td>
-                    <div>
-                        {market && market.name}
-                    </div>
-                </td>
-                <td>
-                    <span>
-                        <Decimal fixed={market.amount_precision} thousSep=",">
-                            {market.last}
-                        </Decimal>
-                    </span>
-                </td>
-                <td>
-                    <span className={marketChangeColor}>{market.price_change_percent}</span>
-                </td>
-                <td>
-                    <span>
-                        <Decimal fixed={market.amount_precision} thousSep=",">
-                            {market.high}
-                        </Decimal>
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        <Decimal fixed={market.amount_precision} thousSep=",">
-                            {market.low}
-                        </Decimal>
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        <Decimal fixed={market.amount_precision} thousSep=",">
-                            {market.volume}
-                        </Decimal>
-                    </span>
-                </td>
-            </tr>
-        );
-    };
-}
+            <div className="pg-ticker-table__table-wrap">
+                <table className="pg-ticker-table__table">
+                    <thead>
+                        <tr>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.pair'})}</th>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.lastPrice'})}</th>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.change'})}</th>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.high'})}</th>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.low'})}</th>
+                            <th scope="col">{intl.formatMessage({id: 'page.body.marketsTable.header.volume'})}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {markets[0] && markets.map((market, index) => renderItem(props, market, index))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
--- src/containers/MarketsTable/index.tsx
+++ src/containers/MarketsTable/index.tsx
@@ -1,186 +1,104 @@
 import * as React from 'react';
-import { injectIntl } from 'react-intl';
-import { connect, MapDispatchToProps } from 'react-redux';
-import { RouterProps } from 'react-router';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Decimal, TickerTable } from '../../components';
-import { IntlProps } from '../../index';
 import {
     Market,
     marketsFetch,
     marketsTickersFetch,
-    RootState,
     selectMarkets,
     selectMarketTickers,
-    selectUserLoggedIn,
     setCurrentMarket,
-    Ticker,
 } from '../../modules';
-import { rangerConnectFetch, RangerConnectFetch } from '../../modules/public/ranger';
-import { RangerState } from '../../modules/public/ranger/reducer';
+import { rangerConnectFetch } from '../../modules/public/ranger';
 import { selectRanger } from '../../modules/public/ranger/selectors';
 
-interface ReduxProps {
-    markets: Market[];
-    marketTickers: {
-        [key: string]: Ticker,
-    };
-    rangerState: RangerState;
-    loggedIn?: boolean;
-}
+const defaultTicker = {
+    amount: '0.0',
+    last: '0.0',
+    high: '0.0',
+    open: '0.0',
+    low: '0.0',
+    price_change_percent: '+0.00%',
+    volume: '0.0',
+};
 
-interface DispatchProps {
-    fetchMarkets: typeof marketsFetch;
-    rangerConnect: typeof rangerConnectFetch;
-    setCurrentMarket: typeof setCurrentMarket;
-    tickers: typeof marketsTickersFetch;
-}
+const handleRedirectToTrading = (dispatch, history, markets: Market[], id: string) => {
+    const currentMarket: Market | undefined = markets.find(item => item.id === id);
 
-interface State {
-    currentBidUnit: string;
-}
+    if (currentMarket) {
+        dispatch(setCurrentMarket(currentMarket));
+        history.push(`/trading/${currentMarket.id}`);
+    }
+};
 
-export type Props = ReduxProps & DispatchProps & RouterProps & IntlProps;
-
-class MarketsTableContainer extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            currentBidUnit: '',
-        };
+const formatFilteredMarkets = (list: string[], market: Market) => {
+    if (!list.includes(market.quote_unit)) {
+        list.push(market.quote_unit);
     }
 
-    public componentDidMount(): void {
-        const {rangerState: { connected }} = this.props;
+    return list;
+};
 
-        if (!connected) {
-            this.props.rangerConnect({withAuth: false});
+const MarketsTableComponent = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const markets = useSelector(selectMarkets);
+    const marketTickers = useSelector(selectMarketTickers);
+    const rangerState = useSelector(selectRanger);
+    const [currentBidUnit, setCurrentBidUnit] = React.useState('');
+
+    React.useEffect(() => {
+        if (!rangerState.connected) {
+            dispatch(rangerConnectFetch({ withAuth: false }));
         }
-    }
-
-    public UNSAFE_componentWillMount() {
-        const { markets } = this.props;
 
         if (markets.length === 0) {
-            this.props.fetchMarkets();
-            this.props.tickers();
+            dispatch(marketsFetch());
+            dispatch(marketsTickersFetch());
         }
+
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    let currentBidUnitsList: string[] = [''];
+
+    if (markets.length > 0) {
+        currentBidUnitsList = markets.reduce(formatFilteredMarkets, currentBidUnitsList);
     }
 
-    public UNSAFE_componentWillReceiveProps(next: Props) {
-        const {
-            markets,
-            tickers,
-        } = this.props;
+    let currentBidUnitMarkets = markets;
 
-        if (next.markets.length === 0 && next.markets !== markets) {
-            tickers();
-        }
+    if (currentBidUnit) {
+        currentBidUnitMarkets = markets.length ? markets.filter(market => market.quote_unit === currentBidUnit) : [];
     }
 
-    public render() {
-        const {
-            markets,
-            marketTickers,
-        } = this.props;
-        const { currentBidUnit } = this.state;
-        const defaultTicker = {
-            amount: '0.0',
-            last: '0.0',
-            high: '0.0',
-            open: '0.0',
-            low: '0.0',
-            price_change_percent: '+0.00%',
-            volume: '0.0',
-        };
+    const formattedMarkets = currentBidUnitMarkets.length ? currentBidUnitMarkets.map(market =>
+        ({
+            ...market,
+            last: Decimal.format(Number((marketTickers[market.id] || defaultTicker).last), market.amount_precision),
+            open: Decimal.format(Number((marketTickers[market.id] || defaultTicker).open), market.price_precision),
+            price_change_percent: String((marketTickers[market.id] || defaultTicker).price_change_percent),
+            high: Decimal.format(Number((marketTickers[market.id] || defaultTicker).high), market.amount_precision),
+            low: Decimal.format(Number((marketTickers[market.id] || defaultTicker).low), market.amount_precision),
+            volume: Decimal.format(Number((marketTickers[market.id] || defaultTicker).volume), market.amount_precision),
+        }),
+    ).map(market =>
+        ({
+            ...market,
+            change: Decimal.format((+market.last - +market.open)
+                .toFixed(market.price_precision), market.price_precision),
+        }),
+    ) : [];
 
-        let currentBidUnitsList: string[] = [''];
+    return (
+        <TickerTable
+            currentBidUnit={currentBidUnit}
+            currentBidUnitsList={currentBidUnitsList}
+            markets={formattedMarkets}
+            redirectToTrading={id => handleRedirectToTrading(dispatch, history, markets, id)}
+            setCurrentBidUnit={setCurrentBidUnit}
+        />
+    );
+};
 
-        if (markets.length > 0) {
-            currentBidUnitsList = markets.reduce(this.formatFilteredMarkets, currentBidUnitsList);
-        }
-
-        let currentBidUnitMarkets = markets;
-
-        if (currentBidUnit) {
-            currentBidUnitMarkets = markets.length ? markets.filter(market => market.quote_unit === currentBidUnit) : [];
-        }
-
-        const formattedMarkets = currentBidUnitMarkets.length ? currentBidUnitMarkets.map(market =>
-            ({
-                ...market,
-                last: Decimal.format(Number((marketTickers[market.id] || defaultTicker).last), market.amount_precision),
-                open: Decimal.format(Number((marketTickers[market.id] || defaultTicker).open), market.price_precision),
-                price_change_percent: String((marketTickers[market.id] || defaultTicker).price_change_percent),
-                high: Decimal.format(Number((marketTickers[market.id] || defaultTicker).high), market.amount_precision),
-                low: Decimal.format(Number((marketTickers[market.id] || defaultTicker).low), market.amount_precision),
-                volume: Decimal.format(Number((marketTickers[market.id] || defaultTicker).volume), market.amount_precision),
-            }),
-        ).map(market =>
-            ({
-                ...market,
-                change: Decimal.format((+market.last - +market.open)
-                    .toFixed(market.price_precision), market.price_precision),
-            }),
-        ) : [];
-
-        return (
-            <TickerTable
-                currentBidUnit={currentBidUnit}
-                currentBidUnitsList={currentBidUnitsList}
-                markets={formattedMarkets}
-                redirectToTrading={this.handleRedirectToTrading}
-                setCurrentBidUnit={this.handleSetCurrentBidUnit}
-                translate={this.translate}
-            />
-        );
-    }
-
-    private handleRedirectToTrading = (id: string) => {
-        const { markets } = this.props;
-        const currentMarket: Market | undefined = markets.find(item => item.id === id);
-
-        if (currentMarket) {
-            this.props.setCurrentMarket(currentMarket);
-            this.props.history.push(`/trading/${currentMarket.id}`);
-        }
-    };
-
-    private handleSetCurrentBidUnit = (currentBidUnit?: string) => {
-        this.setState({
-            currentBidUnit: currentBidUnit || '',
-        });
-    };
-
-    private formatFilteredMarkets = (list: string[], market: Market) => {
-        if (list.indexOf(market.quote_unit) === -1) {
-            list.push(market.quote_unit);
-        }
-
-        return list;
-    };
-
-    private translate = (key: string) => this.props.intl.formatMessage({id: key});
-}
-
-const mapStateToProps = (state: RootState): ReduxProps => ({
-    loggedIn: selectUserLoggedIn(state),
-    markets: selectMarkets(state),
-    marketTickers: selectMarketTickers(state),
-    rangerState: selectRanger(state),
-});
-
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = dispatch => ({
-    fetchMarkets: () => dispatch(marketsFetch()),
-    rangerConnect: (payload: RangerConnectFetch['payload']) => dispatch(rangerConnectFetch(payload)),
-    setCurrentMarket: (market: Market) => dispatch(setCurrentMarket(market)),
-    tickers: () => dispatch(marketsTickersFetch()),
-});
-
-export const MarketsTable = compose(
-    injectIntl,
-    withRouter,
-    connect(mapStateToProps, mapDispatchToProps),
-)(MarketsTableContainer) as React.ComponentClass;
+export const MarketsTable = React.memo(MarketsTableComponent);
