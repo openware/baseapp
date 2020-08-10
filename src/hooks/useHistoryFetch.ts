0211b0ad75b6856c1cc9476481cb50bff5db4214
vposudevskyi <vposudevskyi@star.global>
2020-08-10 18:46:39 +0300

    Feature: Add default values for a hook

--- src/hooks/useHistoryFetch.ts
+++ src/hooks/useHistoryFetch.ts
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchHistory } from '../modules';
 
-export const useHistoryFetch = ({ type, currency }) => {
+export const useHistoryFetch = ({ type, currency, limit = 6, page = 0 }) => {
     const dispatch = useDispatch();
 
     React.useEffect(() => {
-        dispatch(fetchHistory({ type, limit: 6, currency, page: 0 }));
+        dispatch(fetchHistory({ type, limit, currency, page }));
     }, [dispatch, type, currency]);
 };
