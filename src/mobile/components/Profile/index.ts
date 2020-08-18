2474ed1f62c565298e2679f63cf4808c4604804f
oyershov <oyershov@heliostech.fr>
2020-08-18 13:21:24 +0300

    Feature: add mobile user activity

--- src/mobile/components/Profile/index.ts
+++ src/mobile/components/Profile/index.ts
@@ -1,2 +1,3 @@
 export * from './ProfileLinks';
+export * from './UserActivityItem';
 export * from './UserInfo';
---
+++ src/mobile/screens/ProfileAccountActivity/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useIntl } from 'react-intl';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { useUserActivityFetch } from '../../../hooks';
+import { selectUserActivity } from '../../../modules';
+import { Subheader, UserActivityItem } from '../../components';
+
+const ProfileAccountActivityMobileScreenComponent = () => {
+    const intl = useIntl();
+    const history = useHistory();
+    const userActivity = useSelector(selectUserActivity);
+    useUserActivityFetch({});
+
+    return (
+        <React.Fragment>
+          <Subheader
+            title={intl.formatMessage({ id: 'page.mobile.profile.accountActivity.title' })}
+            backTitle={intl.formatMessage({ id: 'page.body.profile.header.account' })}
+            onGoBack={() => history.push('/profile')}
+          />
+            <div className="pg-mobile-profile-account-activity-screen">
+                <div className="pg-mobile-profile-account-activity-screen__list">
+                    {userActivity.length ? (
+                        userActivity.map((item, index) => <UserActivityItem key={index} item={item} />)
+                    ) : (
+                        <span className="no-data">{intl.formatMessage({id: 'page.noDataToShow'})}</span>
+                    )}
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export const ProfileAccountActivityMobileScreen = React.memo(ProfileAccountActivityMobileScreenComponent);
