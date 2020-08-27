6a96669a108a327da2a8b909a5b03b5dd017d6bf
oyershov <oyershov@heliostech.fr>
2020-08-27 15:22:30 +0300

    Fix: add beneficiaries fetch on update

--- src/hooks/useBeneficiariesFetch.ts
+++ src/hooks/useBeneficiariesFetch.ts
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
-import { beneficiariesFetch } from '../modules';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    beneficiariesFetch,
+    selectBeneficiariesActivateSuccess,
+    selectBeneficiariesDeleteSuccess,
+} from '../modules';
 
 export const useBeneficiariesFetch = () => {
     const dispatch = useDispatch();
+    const beneficiariesActivateSuccess = useSelector(selectBeneficiariesActivateSuccess);
+    const beneficiariesDeleteSuccess = useSelector(selectBeneficiariesDeleteSuccess);
 
     React.useEffect(() => {
         dispatch(beneficiariesFetch());
     }, [dispatch]);
+
+    React.useEffect(() => {
+        if (beneficiariesActivateSuccess || beneficiariesDeleteSuccess) {
+            dispatch(beneficiariesFetch());
+        }
+    }, [
+        dispatch,
+        beneficiariesActivateSuccess,
+        beneficiariesDeleteSuccess,
+    ]);
 };
