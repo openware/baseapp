5790e66c19f4d7d0394805f78b4acf55c1753268
Vladyslav Posudevskyi <vladyslav_95@ukr.net>
2020-09-07 18:45:20 +0300

    Feature: Remove some tests

--- src/components/Markets/Markets.test.tsx
+++ src/components/Markets/Markets.test.tsx
@@ -34,26 +34,26 @@ describe.skip('Markets', () => {
        expect(wrapper).toMatchSnapshot();
    });
 
-   it('should correctly filter rows', () => {
-       expect((wrapper.instance() as any).searchFilter(['ETH/BTC', '0.123', '+50.00%'], 'btc')).toBeTruthy();
-       expect((wrapper.instance() as any).searchFilter(['ETH/BTC', '0.342', '+50.00%'], 'ltc')).toBeFalsy();
-   });
+   // it('should correctly filter rows', () => {
+   //     expect((wrapper.instance() as any).searchFilter(['ETH/BTC', '0.123', '+50.00%'], 'btc')).toBeTruthy();
+   //     expect((wrapper.instance() as any).searchFilter(['ETH/BTC', '0.342', '+50.00%'], 'ltc')).toBeFalsy();
+   // });
 
-   it('should set filtered data to state', () => {
-      const component: ReactWrapper = mount(
-          <Markets data={data} onSelect={onSelect} />,
-      );
-
-      const filteredData = [
-          ['ETH/BTC', '0.123', '+50.00%'],
-      ];
-
-      (component.instance() as any).handleFilter(filteredData as object[]);
-      const input = component.find('tr').first();
-      input.simulate('click');
-
-      expect((component.state() as any).filteredData).toEqual(filteredData);
-   });
+   // it('should set filtered data to state', () => {
+   //    const component: ReactWrapper = mount(
+   //        <Markets data={data} onSelect={onSelect} />,
+   //    );
+   //
+   //    const filteredData = [
+   //        ['ETH/BTC', '0.123', '+50.00%'],
+   //    ];
+   //
+   //    (component.instance() as any).handleFilter(filteredData as object[]);
+   //    const input = component.find('tr').first();
+   //    input.simulate('click');
+   //
+   //    expect((component.state() as any).filteredData).toEqual(filteredData);
+   // });
 
    it('should set selected market in props', () => {
       const keyIndex = 0;
@@ -66,16 +66,16 @@ describe.skip('Markets', () => {
       expect(resultSelectedRow).toBe(expectedSelectedRow);
    });
 
-   it('should set new data to state', () => {
-       const component: ReactWrapper = mount(
-           <Markets data={data} onSelect={onSelect} />,
-       );
-
-       const filteredData = [
-           ['ETH/BTC', '0.123', '+50.00%'],
-       ];
-
-       component.setProps({ data: filteredData });
-       expect((component.state() as any).filteredData).toEqual(filteredData);
-   });
+   // it('should set new data to state', () => {
+   //     const component: ReactWrapper = mount(
+   //         <Markets data={data} onSelect={onSelect} />,
+   //     );
+   //
+   //     const filteredData = [
+   //         ['ETH/BTC', '0.123', '+50.00%'],
+   //     ];
+   //
+   //     component.setProps({ data: filteredData });
+   //     expect((component.state() as any).filteredData).toEqual(filteredData);
+   // });
 });
