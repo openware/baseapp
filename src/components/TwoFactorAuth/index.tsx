d4311435c1a41d01082d1c177d889ee4aa87434f
Vladyslav Posudevskyi <vladyslav_95@ukr.net>
2020-10-05 13:55:19 +0300

    Feature: Move TwoFactorAuth component to func component

--- src/components/TwoFactorAuth/index.tsx
+++ src/components/TwoFactorAuth/index.tsx
@@ -20,101 +20,86 @@ export interface TwoFactorAuthProps {
     handleClose2fa: () => void;
 }
 
-class TwoFactorAuthComponent extends React.Component<TwoFactorAuthProps> {
-    public render() {
-        const {
-            errorMessage,
-            isLoading,
-            title,
-            label,
-            buttonLabel,
-            message,
-            error,
-            otpCode,
-            codeFocused,
-        } = this.props;
+export const TwoFactorAuth: React.FC<TwoFactorAuthProps> = (props: TwoFactorAuthProps) => {
+    const {
+        errorMessage,
+        isLoading,
+        title,
+        label,
+        buttonLabel,
+        message,
+        error,
+        otpCode,
+        codeFocused,
+        onSubmit,
+    } = props;
 
-        const errors = errorMessage || error;
-        const buttonWrapperClass = cr('cr-email-form__button-wrapper', {
-            'cr-email-form__button-wrapper--empty': !errors,
-        });
-        const emailGroupClass = cr('cr-email-form__group', {
-            'cr-email-form__group--focused': codeFocused,
-        });
+    const buttonWrapperClass = React.useMemo(() => cr('cr-email-form__button-wrapper', {
+        'cr-email-form__button-wrapper--empty': !(errorMessage || error),
+    }), [errorMessage, error]);
 
-        return (
-            <div className="pg-2fa___form">
-                <form>
-                    <div className="cr-email-form">
-                        <div className="cr-email-form__options-group">
-                            <div className="cr-email-form__option">
-                                <div className="cr-email-form__option-inner">
-                                    {title || '2FA verification'}
-                                    <div className="cr-email-form__cros-icon" onClick={this.handleCancel}>
-                                        <CloseIcon className="close-icon" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cr-email-form__form-content">
-                            <div className="cr-email-form__header">
-                              {message}
-                            </div>
-                            <div className={emailGroupClass}>
-                                <CustomInput
-                                    type="number"
-                                    label={label || '6-digit Google Authenticator Code'}
-                                    placeholder={label || '6-digit Google Authenticator Code'}
-                                    defaultLabel="6-digit Google Authenticator Code"
-                                    handleChangeInput={this.props.handleOtpCodeChange}
-                                    inputValue={otpCode}
-                                    handleFocusInput={this.props.handleChangeFocusField}
-                                    classNameLabel="cr-email-form__label"
-                                    classNameInput="cr-email-form__input"
-                                    onKeyPress={this.handleEnterPress}
-                                    autoFocus={true}
-                                />
-                                {errorMessage && <div className="cr-email-form__error">{errorMessage}</div>}
-                            </div>
-                            <div className={buttonWrapperClass}>
-                                <Button
-                                    disabled={isLoading || !otpCode.match(`^[0-9]{6}$`)}
-                                    onClick={this.handleSubmit}
-                                    size="lg"
-                                    variant="primary"
-                                >
-                                    {isLoading ? 'Loading...' : (buttonLabel ? buttonLabel : 'Sign in')}
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+    const emailGroupClass = React.useMemo(() => cr('cr-email-form__group', {
+        'cr-email-form__group--focused': codeFocused,
+    }), [codeFocused]);
 
-    private handleCancel = () => {
-        this.props.handleClose2fa();
-    };
 
-    private handleSubmit = () => {
-        this.props.onSubmit();
-    };
-
-    private handleEnterPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        const {
-            isLoading,
-            otpCode,
-        } = this.props;
-
+    const handleEnterPress = React.useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
             event.preventDefault();
 
             if (!isLoading && otpCode.match(`^[0-9]{6}$`)) {
-                this.handleSubmit();
+                onSubmit();
             }
         }
-    };
-}
+    }, [onSubmit, otpCode, isLoading]);
 
-export const TwoFactorAuth = TwoFactorAuthComponent;
+    return (
+        <div className="pg-2fa___form">
+            <form>
+                <div className="cr-email-form">
+                    <div className="cr-email-form__options-group">
+                        <div className="cr-email-form__option">
+                            <div className="cr-email-form__option-inner">
+                                {title || '2FA verification'}
+                                <div className="cr-email-form__cros-icon" onClick={props.handleClose2fa}>
+                                    <CloseIcon className="close-icon" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cr-email-form__form-content">
+                        <div className="cr-email-form__header">
+                            {message}
+                        </div>
+                        <div className={emailGroupClass}>
+                            <CustomInput
+                                type="number"
+                                label={label || '6-digit Google Authenticator Code'}
+                                placeholder={label || '6-digit Google Authenticator Code'}
+                                defaultLabel="6-digit Google Authenticator Code"
+                                handleChangeInput={props.handleOtpCodeChange}
+                                inputValue={otpCode}
+                                handleFocusInput={props.handleChangeFocusField}
+                                classNameLabel="cr-email-form__label"
+                                classNameInput="cr-email-form__input"
+                                onKeyPress={handleEnterPress}
+                                autoFocus={true}
+                            />
+                            {errorMessage && <div className="cr-email-form__error">{errorMessage}</div>}
+                        </div>
+                        <div className={buttonWrapperClass}>
+                            <Button
+                                disabled={isLoading || !otpCode.match(`^[0-9]{6}$`)}
+                                onClick={props.onSubmit}
+                                size="lg"
+                                variant="primary"
+                            >
+                                {isLoading ? 'Loading...' : (buttonLabel ? buttonLabel : 'Sign in')}
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
