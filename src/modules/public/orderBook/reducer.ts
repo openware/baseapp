07654ca40c3801e0e13c7950b609e3ba20872fc6
pkhlopiachyi <khlpavlo@gmail.com>
2020-10-09 12:49:06 +0300

    Fix: filter zero amount asks and bids

--- src/modules/public/orderBook/reducer.ts
+++ src/modules/public/orderBook/reducer.ts
@@ -116,17 +116,17 @@ export const incrementDepthReducer = (state = initialIncrementDepth, action: Dep
 
             if (action.payload.asks) {
                 payload.asks = Array.isArray(action.payload.asks[0]) ? (
-                    handleIncrementalUpdateArray(state.asks, action.payload.asks as string[][], 'asks').slice(0, orderBookSideLimit())
+                    handleIncrementalUpdateArray(state.asks, action.payload.asks as string[][], 'asks').slice(0, orderBookSideLimit()).filter(ask => ask[1])
                 ) : (
-                    handleIncrementalUpdate(state.asks, action.payload.asks as string[], 'asks').slice(0, orderBookSideLimit())
+                    handleIncrementalUpdate(state.asks, action.payload.asks as string[], 'asks').slice(0, orderBookSideLimit()).filter(ask => ask[1])
                 );
             }
 
             if (action.payload.bids) {
                 payload.bids = Array.isArray(action.payload.bids[0]) ? (
-                    handleIncrementalUpdateArray(state.bids, action.payload.bids as string[][], 'bids').slice(0, orderBookSideLimit())
+                    handleIncrementalUpdateArray(state.bids, action.payload.bids as string[][], 'bids').slice(0, orderBookSideLimit()).filter(bid => bid[1])
                 ) : (
-                    handleIncrementalUpdate(state.bids, action.payload.bids as string[], 'bids').slice(0, orderBookSideLimit())
+                    handleIncrementalUpdate(state.bids, action.payload.bids as string[], 'bids').slice(0, orderBookSideLimit()).filter(bid => bid[1])
                 );
             }
 
