b9410ca0dd79f137f28a55b37bf461ded73bb085
Andrew Peresada <kohelbekker@gmail.com>
2020-10-19 12:07:45 +0300

    Feature: add SignIn 2FA, SignUp loaders (#360)

--- src/modules/user/auth/sagas/signUpSaga.ts
+++ src/modules/user/auth/sagas/signUpSaga.ts
@@ -2,7 +2,7 @@
 import { call, put } from 'redux-saga/effects';
 import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
-import { signUpError, SignUpFetch, signUpRequireVerification } from '../actions';
+import { signUpData, signUpError, SignUpFetch, signUpRequireVerification } from '../actions';
 
 const signUpConfig: RequestOptions = {
     apiVersion: 'barong',
@@ -12,6 +12,7 @@ export function* signUpSaga(action: SignUpFetch) {
     try {
         yield call(API.post(signUpConfig), '/identity/users', action.payload);
         yield put(signUpRequireVerification({ requireVerification: true }));
+        yield put(signUpData());
     } catch (error) {
         yield put(signUpError(error));
         yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
