a53d5c11ce0c4e9f25936a072b57d59048985464
Vladyslav Posudevskyi <18016868+VladyslavP@users.noreply.github.com>
2020-10-19 12:08:17 +0300

    Feature: Refactor beneficiaries (#355)
    * Feature: Move ActivateModal & FailModal to func components
    
    * Feature: Add hooks to activate
    
    * Fix: Run linter
    
    * Fix: Close modal on activation & onFocus

--- src/components/Beneficiaries/BeneficiariesFailAddModal.tsx
+++ src/components/Beneficiaries/BeneficiariesFailAddModal.tsx
@@ -1,51 +1,38 @@
 import classnames from 'classnames';
-import { History } from 'history';
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
-import { injectIntl } from 'react-intl';
-import { Link, RouteProps, withRouter } from 'react-router-dom';
-import { IntlProps } from '../../index';
+import { useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
 import { Modal } from '../../mobile/components/Modal';
 
-interface OwnProps {
+interface Props {
     handleToggleFailModal: () => void;
-    history: History;
     isMobileDevice?: boolean;
 }
 
-type Props = OwnProps & IntlProps & RouteProps;
 
-class BeneficiariesFailAddModalComponent extends React.Component<Props> {
-    public render() {
-        return (
-            this.props.isMobileDevice ?
-                <Modal
-                    isOpen
-                    onClose={this.props.handleToggleFailModal}
-                    title={this.translate('page.body.wallets.beneficiaries.failAddModal.content')}>
-                    {this.renderContent()}
-                </Modal> : this.renderContent()
-        );
-    }
+const BeneficiariesFailAddModalComponent: React.FC<Props> = (props: Props) => {
+    const { formatMessage } = useIntl();
+    const { handleToggleFailModal, isMobileDevice } = props;
 
-    public ModalHeader = () => (
+    const ModalHeader = React.useCallback(() => (
         <div className="cr-email-form__options-group">
             <div className="cr-email-form__option">
                 <div className="cr-email-form__option-inner">
-                    {this.translate('page.body.wallets.beneficiaries.failAddModal.header')}
+                    {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.header' })}
                     <span
                         className="pg-profile-page__close pg-profile-page__pull-right"
-                        onClick={this.props.handleToggleFailModal}
+                        onClick={handleToggleFailModal}
                     />
                 </div>
             </div>
         </div>
-    );
+    ), [handleToggleFailModal, formatMessage]);
 
-    public ModalBody = () => (
+    const ModalBody = React.useCallback(() => (
         <div className="cr-email-form__form-content">
             <span className="cr-email-form__form-content__info">
-                {this.translate('page.body.wallets.beneficiaries.failAddModal.content')}
+                {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.content' })}
             </span>
             <div className="cr-email-form__button-wrapper">
                 <Link to="/confirm">
@@ -54,30 +41,41 @@ class BeneficiariesFailAddModalComponent extends React.Component<Props> {
                         variant="primary"
                         className="cr-email-form__button-wrapper__btn"
                     >
-                        {this.translate('page.body.wallets.beneficiaries.failAddModal.button')}
+                        {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.button' })}
                     </Button>
                 </Link>
             </div>
         </div>
-    );
+    ), [formatMessage]);
 
-    private renderContent = () => {
+    const renderContent = React.useCallback(() => {
         const className = classnames('beneficiaries-fail-modal', {
-            'cr-modal': !this.props.isMobileDevice,
+            'cr-modal': !isMobileDevice,
         });
 
         return (
             <div className={className}>
                 <div className="cr-email-form">
-                    <this.ModalHeader/>
-                    <this.ModalBody/>
+                    <ModalHeader/>
+                    <ModalBody/>
                 </div>
             </div>
         );
-    };
+    }, [isMobileDevice]);
 
-    private translate = (id: string) => this.props.intl.formatMessage({ id });
-}
+    return (
+        props.isMobileDevice ?
+            <Modal
+                isOpen
+                onClose={props.handleToggleFailModal}
+                title={formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.content' })}>
+                {renderContent()}
+            </Modal> : renderContent()
+    );
+};
 
-// tslint:disable-next-line:no-any
-export const BeneficiariesFailAddModal = withRouter(injectIntl(BeneficiariesFailAddModalComponent) as any) as any;
+const BeneficiariesFailAddModal = React.memo(BeneficiariesFailAddModalComponent);
+
+export {
+    BeneficiariesFailAddModal,
+};
