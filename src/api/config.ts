3135a352e5adb36d04706230c013d6f1bf9c5f68
oyershov <oyershov@heliostech.fr>
2020-12-22 18:29:51 +0100

    Fix: prevent crashes on missing environment variables

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,79 +1,66 @@
-import {
-    DEFAULT_KYC_STEPS,
-    ORDER_BOOK_DEFAULT_SIDE_LIMIT,
-    STORAGE_DEFAULT_LIMIT,
-} from '../constants';
-import { Config } from './types';
-
 export const defaultConfig: Config = {
     api: {
         authUrl: '',
         tradeUrl: '',
         applogicUrl: '',
         rangerUrl: '',
-        finexUrl: '',
+        finexUrl: ''
     },
-    minutesUntilAutoLogout: '5',
-    rangerReconnectPeriod: '1',
-    withCredentials: true,
-    storage: {},
-    gaTrackerKey: '',
-    msAlertDisplayTime: '5000',
-    incrementalOrderBook: false,
     finex: false,
+    withCredentials: false,
+    incrementalOrderBook: false,
     isResizable: false,
     isDraggable: false,
-    languages: ['en'],
-    sessionCheckInterval: '15000',
-    balancesFetchInterval: '3000',
-    passwordEntropyStep: 0,
     showLanding: true,
     sentryEnabled: false,
+    captchaLogin: false,
+    gaTrackerKey: '',
+    minutesUntilAutoLogout: '5',
+    msAlertDisplayTime: '5000',
+    sessionCheckInterval: '15000',
+    balancesFetchInterval: '3000',
+    passwordEntropyStep: '14',
+    storage: {
+        defaultStorageLimit: '50',
+        orderBookSideLimit: '25'
+    },
+    languages: ['en', 'ru'],
     kycSteps: [
         'email',
         'phone',
         'profile',
         'document',
-        'address',
-    ],
-    captchaLogin: false,
+        'address'
+    ]
 };
 
 export const Cryptobase = {
     config: defaultConfig,
 };
 
-declare global {
-    interface Window {
-        env: Config;
-    }
-}
+Cryptobase.config = { ...defaultConfig, ...window.env };
+Cryptobase.config.storage = { ...defaultConfig.storage, ...Cryptobase.config.storage };
 
-window.env = window.env || defaultConfig;
-Cryptobase.config = { ...window.env };
-Cryptobase.config.storage = Cryptobase.config.storage || {};
-
-export const tradeUrl = () => Cryptobase.config.api.tradeUrl;
-export const authUrl = () => Cryptobase.config.api.authUrl;
-export const applogicUrl = () => Cryptobase.config.api.applogicUrl;
-export const rangerUrl = () => Cryptobase.config.api.rangerUrl;
-export const finexUrl = () => Cryptobase.config.api.finexUrl || tradeUrl();
-export const minutesUntilAutoLogout = (): string => Cryptobase.config.minutesUntilAutoLogout || '5';
-export const withCredentials = () => Cryptobase.config.withCredentials;
-export const defaultStorageLimit = () => Cryptobase.config.storage.defaultStorageLimit || STORAGE_DEFAULT_LIMIT;
-export const orderBookSideLimit = () => Cryptobase.config.storage.orderBookSideLimit || ORDER_BOOK_DEFAULT_SIDE_LIMIT;
-export const gaTrackerKey = (): string => Cryptobase.config.gaTrackerKey || '';
-export const msAlertDisplayTime = (): string => Cryptobase.config.msAlertDisplayTime || '5000';
-export const rangerReconnectPeriod = (): number => Cryptobase.config.rangerReconnectPeriod ? Number(Cryptobase.config.rangerReconnectPeriod) : 1;
-export const incrementalOrderBook = (): boolean => Cryptobase.config.incrementalOrderBook || false;
-export const isResizableGrid = ():boolean => Cryptobase.config.isResizable || false;
-export const isDraggableGrid = ():boolean => Cryptobase.config.isDraggable || false;
-export const languages = Cryptobase.config.languages && Cryptobase.config.languages.length > 0 ? Cryptobase.config.languages : ['en'];
-export const sessionCheckInterval = (): string => Cryptobase.config.sessionCheckInterval || '15000';
-export const balancesFetchInterval = (): string => Cryptobase.config.balancesFetchInterval || '3000';
-export const isFinexEnabled = (): boolean => Cryptobase.config.finex || false;
-export const passwordEntropyStep = ():number => Cryptobase.config.passwordEntropyStep;
+export const tradeUrl = (): string => Cryptobase.config.api.tradeUrl;
+export const authUrl = (): string => Cryptobase.config.api.authUrl;
+export const applogicUrl = (): string => Cryptobase.config.api.applogicUrl;
+export const rangerUrl = (): string => Cryptobase.config.api.rangerUrl;
+export const finexUrl = (): string => Cryptobase.config.api.finexUrl || tradeUrl();
+export const withCredentials = (): boolean => Cryptobase.config.withCredentials;
+export const incrementalOrderBook = (): boolean => Cryptobase.config.incrementalOrderBook;
+export const isResizableGrid = (): boolean => Cryptobase.config.isResizable;
+export const isDraggableGrid = (): boolean => Cryptobase.config.isDraggable;
+export const isFinexEnabled = (): boolean => Cryptobase.config.finex ;
 export const showLanding = (): boolean => Cryptobase.config.showLanding;
-export const sentryEnabled = () => Cryptobase.config.sentryEnabled || defaultConfig.sentryEnabled;
-export const kycSteps = (): string[] => Cryptobase.config.kycSteps || DEFAULT_KYC_STEPS;
-export const captchaLogin = (): boolean => Cryptobase.config.captchaLogin || false;
+export const sentryEnabled = (): boolean => Cryptobase.config.sentryEnabled;
+export const captchaLogin = (): boolean => Cryptobase.config.captchaLogin;
+export const minutesUntilAutoLogout = (): string => Cryptobase.config.minutesUntilAutoLogout;
+export const sessionCheckInterval = (): string => Cryptobase.config.sessionCheckInterval;
+export const balancesFetchInterval = (): string => Cryptobase.config.balancesFetchInterval;
+export const gaTrackerKey = (): string => Cryptobase.config.gaTrackerKey;
+export const msAlertDisplayTime = (): string => Cryptobase.config.msAlertDisplayTime;
+export const defaultStorageLimit = (): number => Number(Cryptobase.config.storage.defaultStorageLimit);
+export const orderBookSideLimit = (): number => Number(Cryptobase.config.storage.orderBookSideLimit);
+export const passwordEntropyStep = (): number => Number(Cryptobase.config.passwordEntropyStep);
+export const languages: string[] = (Cryptobase.config.languages && Cryptobase.config.languages.length > 0) ? Cryptobase.config.languages : ['en'];
+export const kycSteps = (): string[] => Cryptobase.config.kycSteps;
