ebfa374717e3905ee5cf1b7902035d754f87d531
oyershov <oyershov@heliostech.fr>
2021-01-20 12:56:45 +0100

    Fix: solve translations overwriting issue

--- src/custom/translations/index.ts
+++ src/custom/translations/index.ts
@@ -1,13 +1,4 @@
-import { en } from '../../translations/en';
 import { ru } from './ru';
-
-// tslint:disable:no-submodule-imports
-// import localeRu from 'react-intl/locale-data/ru';
-// tslint:enable
-
-// export const customLocaleData = ([...localeRu]);
-
-export type LangType = typeof en;
 
 export const customLanguageMap = {
     ru,
--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,10 +1,11 @@
 import { customLanguageMap } from '../custom/translations';
+import { en as enCustom } from '../custom/translations/en';
 import { en } from './en';
 
 export type LangType = typeof en;
 
 export const languageMap = {
     default: en,
-    en,
+    en: { ...en, ...enCustom },
     ...customLanguageMap,
 };
