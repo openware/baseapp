9e0a68add3da8e2c551cfbc6adf989aaea56e267
Sean Lee <5255084-seanlee1@users.noreply.gitlab.com>
2021-01-21 17:09:12 +0700

    Fix: phone verification sendcode logic should cover resend too

--- src/modules/user/kyc/phone/sagas/sendCodeSaga.ts
+++ src/modules/user/kyc/phone/sagas/sendCodeSaga.ts
@@ -2,7 +2,13 @@ import { call, put } from 'redux-saga/effects';
 import { alertPush, sendError } from '../../../../';
 import { API, RequestOptions } from '../../../../../api';
 import { getCsrfToken } from '../../../../../helpers';
-import { sendCodeData, sendCodeError, SendCodeFetch } from '../actions';
+import {
+    resendCodeData,
+    resendCodeError,
+    sendCodeData,
+    sendCodeError,
+    SendCodeFetch,
+} from '../actions';
 
 const sessionsConfig = (csrfToken?: string): RequestOptions => {
     return {
@@ -17,7 +23,21 @@ export function* sendCodeSaga(action: SendCodeFetch) {
         yield put(sendCodeData());
         yield put(alertPush({message: ['success.phone.verification.send'], type: 'success'}));
     } catch (error) {
-        if (error.message.indexOf('resource.phone.number_exist') > -1) {
+        if (error.message.indexOf('resource.phone.exists') > -1) {
+            try {
+                yield call(API.post(sessionsConfig(getCsrfToken())), '/resource/phones/send_code', action.payload);
+                yield put(resendCodeData());
+                yield put(alertPush({ message: ['success.phone.verification.send'], type: 'success'}));
+            } catch (error) {
+                yield put(sendError({
+                    error,
+                    processingType: 'alert',
+                    extraOptions: {
+                        actionError: resendCodeError,
+                    },
+                }));
+            }
+        } else if (error.message.indexOf('resource.phone.number_exist') > -1) {
             yield put(alertPush({ message: error.message, type: 'success' }));
         } else {
             yield put(sendError({
