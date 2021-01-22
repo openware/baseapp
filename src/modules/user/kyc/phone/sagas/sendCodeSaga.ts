2f774eba730f1cdb14c0ad4609fa0db20bd1c914
seanyellowd <67998969+seanyellowd@users.noreply.github.com>
2021-01-22 17:02:21 +0700

    Fix: phone verification sendcode logic should cover resend too (#420)
    * Fix: phone verification sendcode logic should cover resend too
    
    * Fix: resend code if unverified phone_number exists
    
    Co-authored-by: Sean Lee <5255084-seanlee1@users.noreply.gitlab.com>

--- src/modules/user/kyc/phone/sagas/sendCodeSaga.ts
+++ src/modules/user/kyc/phone/sagas/sendCodeSaga.ts
@@ -2,7 +2,12 @@ import { call, put } from 'redux-saga/effects';
 import { alertPush, sendError } from '../../../../';
 import { API, RequestOptions } from '../../../../../api';
 import { getCsrfToken } from '../../../../../helpers';
-import { sendCodeData, sendCodeError, SendCodeFetch } from '../actions';
+import {
+    resendCode,
+    sendCodeData,
+    sendCodeError,
+    SendCodeFetch,
+} from '../actions';
 
 const sessionsConfig = (csrfToken?: string): RequestOptions => {
     return {
@@ -17,8 +22,8 @@ export function* sendCodeSaga(action: SendCodeFetch) {
         yield put(sendCodeData());
         yield put(alertPush({message: ['success.phone.verification.send'], type: 'success'}));
     } catch (error) {
-        if (error.message.indexOf('resource.phone.number_exist') > -1) {
-            yield put(alertPush({ message: error.message, type: 'success' }));
+        if (error.message.indexOf('resource.phone.exists') > -1) {
+            yield put(resendCode(action.payload));
         } else {
             yield put(sendError({
                 error,
