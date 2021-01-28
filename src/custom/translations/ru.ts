e2f6f6594929bf67357bdf1f27280739871baae9
Sean Lee <5255084-seanlee1@users.noreply.gitlab.com>
2021-01-28 09:33:15 +0700

    Fix: profile kyc naming

--- src/custom/translations/ru.ts
+++ src/custom/translations/ru.ts
@@ -459,7 +459,7 @@ export const ru: LangType = {
     /* KYC */
     'page.confirm.title.email': 'Get Verified your email',
     'page.confirm.title.phone': 'Get Verified your phone number',
-    'page.confirm.title.profile': 'Get Verified your Identity',
+    'page.confirm.title.profile': 'Get Verified your Profile',
     'page.confirm.title.document': 'Get Verified your Government Issued ID ',
     'page.confirm.title.address': 'Residence verification',
 
