e0979e38e427c27eb78d486764a45f3bce6cdbbe
josadcha <39953699+josadcha@users.noreply.github.com>
2021-01-28 15:52:35 +0200

    Fix: polish Recent Trades after wrong rebase (#439)

--- src/containers/RecentTrades/RecentTradesTableCell.tsx
+++ src/containers/RecentTrades/RecentTradesTableCell.tsx
@@ -3,15 +3,15 @@ import { Decimal } from '../../components/Decimal';
 import { setTradeColor } from '../../helpers';
 
 const TradeTableCellComponent = props => {
-    const { type, takerType, higlightedDate, amountFixed, amount, priceFixed, price, prevValue } = props;
+    const { type, takerType, higlightedDate, amountFixed, amount, priceFixed, price, prevValue, id } = props;
 
     switch (type) {
         case 'date':
-            return <span style={{ color: setTradeColor(takerType).color }}>{higlightedDate}</span>;
+            return <span style={{ color: setTradeColor(takerType).color }} key={id}>{higlightedDate}</span>;
         case 'amount':
-            return <span style={{ color: setTradeColor(takerType).color }}><Decimal fixed={amountFixed}>{amount}</Decimal></span>;
+            return <span style={{ color: setTradeColor(takerType).color }}><Decimal fixed={amountFixed} thousSep="," key={id}>{amount}</Decimal></span>;
         case 'price':
-            return <span style={{ color: setTradeColor(takerType).color }}><Decimal fixed={priceFixed} prevValue={prevValue}>{price}</Decimal></span>;
+            return <span style={{ color: setTradeColor(takerType).color }}><Decimal fixed={priceFixed} thousSep="," prevValue={prevValue} key={id}>{price}</Decimal></span>;
         default:
             return <span />;
     }
