90b07a72760feeed651cfd46f9ca3a51ac877d36
Vadym Poberezhnyy <vpoberezhnyi@heliostech.fr>
2021-01-29 11:46:13 +0200

    fix: fix buiild script to not be freezed

--- webpack/production.ts
+++ webpack/production.ts
@@ -5,9 +5,7 @@ import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import path from 'path';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import JavaScriptObfuscator from 'webpack-obfuscator';
-// import CompressionPlugin from 'compression-webpack-plugin';
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const rootDir = path.resolve(__dirname, '..');
 const BUILD_DIR = path.resolve(rootDir, 'build');
@@ -15,6 +13,27 @@ const BUILD_DIR = path.resolve(rootDir, 'build');
 import commonConfig from './common';
 
 const domain = process.env.BUILD_DOMAIN ? process.env.BUILD_DOMAIN.split(',') : [];
+
+const plugins = [
+    new ExtendedAPIPlugin(),
+    new DefinePlugin({ 'process.env.BUILD_EXPIRE': JSON.stringify(process.env.BUILD_EXPIRE) }),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }],
+        },
+        canPrint: false,
+    }),
+    new CopyWebpackPlugin({
+        patterns: [{ from: 'public' }],
+    }),
+    new JavaScriptObfuscator({ rotateUnicodeArray: true, domainLock: domain }),
+];
+
+if (process.env.ANALYZE === '1') {
+    plugins.push(new BundleAnalyzerPlugin());
+}
 
 const config = merge(commonConfig, {
     mode: 'production',
@@ -27,36 +46,8 @@ const config = merge(commonConfig, {
     optimization: {
         usedExports: false,
         minimize: true,
-        minimizer: [
-            new UglifyJsPlugin({
-                include: /\.min\.js$/,
-            }),
-        ],
     },
-    plugins: [
-        new BundleAnalyzerPlugin(),
-        new ExtendedAPIPlugin(),
-        new DefinePlugin({ 'process.env.BUILD_EXPIRE': JSON.stringify(process.env.BUILD_EXPIRE) }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorPluginOptions: {
-                preset: ['default', { discardComments: { removeAll: true } }],
-            },
-            canPrint: false,
-        }),
-        new CopyWebpackPlugin({
-            patterns: [{ from: 'public' }],
-        }),
-        new JavaScriptObfuscator({ rotateUnicodeArray: true, domainLock: domain }),
-        // new CompressionPlugin({
-        //     filename: '[path].gz[query]',
-        //     algorithm: 'gzip',
-        //     test: /\.js$|\.css$|\.html$/,
-        //     threshold: 10240,
-        //     minRatio: 0.8,
-        // }),
-    ],
+    plugins,
     module: {
         rules: [
             {
