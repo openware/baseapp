ea2e721a5323aa2026b374b2cbb438b2d8869193
oyershov <oyershov@heliostech.fr>
2021-02-12 10:28:02 +0100

    Feature: add synchronous config fetch from sonic

---
+++ public/config.js
@@ -0,0 +1,9 @@
+const fetchConfig = new XMLHttpRequest();
+const hostUrl = window.location.hostname === 'localhost' ? 'http://localhost:9002' : '';
+
+fetchConfig.open('GET', `${hostUrl}/api/v2/sonic/public/config`, false);
+fetchConfig.send(null);
+
+if (fetchConfig.status === 200) {
+  window.env = JSON.parse(fetchConfig.responseText);
+}
