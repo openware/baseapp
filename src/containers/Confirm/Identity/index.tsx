2e1089da4879774dfd195ebc26416f5b80046d4a
crackfoo <info@zpool.ca>
2021-02-16 11:25:55 -0400

    fix postcode regex
    allow alphanumeric postal codes

--- src/containers/Confirm/Identity/index.tsx
+++ src/containers/Confirm/Identity/index.tsx
@@ -380,7 +380,7 @@ class IdentityComponent extends React.Component<Props, IdentityState> {
 
                 return value.match(cityRegex) ? true : false;
             case 'postcode':
-                const postcodeRegex = new RegExp(`^[0-9]{1,12}$`);
+                const postcodeRegex = new RegExp(`^[a-zA-Z0-9]{1,12}$`);
 
                 return value.match(postcodeRegex) ? true : false;
             case 'dateOfBirth':
