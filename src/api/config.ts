4239e721d1782741f6488f8f087bd73d10efbffb
oyershov <oyershov@heliostech.fr>
2021-02-19 15:29:25 +0100

    Fix: handle string type for boolean

--- src/api/config.ts
+++ src/api/config.ts
@@ -34,7 +34,7 @@ export const defaultConfig: Config = {
         'address'
     ],
     captcha_type: 'none',
-    password_min_entropy: 0,
+    password_min_entropy: '0',
 };
 
 export const Cryptobase = {
@@ -44,19 +44,23 @@ export const Cryptobase = {
 Cryptobase.config = { ...defaultConfig, ...window.env };
 Cryptobase.config.storage = { ...defaultConfig.storage, ...Cryptobase.config.storage };
 
+const convertToBoolean = (value: any): boolean => {
+    return String(value) === 'true';
+}
+
 export const tradeUrl = () => Cryptobase.config.api.tradeUrl;
 export const authUrl = () => Cryptobase.config.api.authUrl;
 export const applogicUrl = () => Cryptobase.config.api.applogicUrl;
 export const rangerUrl = () => Cryptobase.config.api.rangerUrl;
 export const finexUrl = () => Cryptobase.config.api.finexUrl || tradeUrl();
-export const withCredentials = () => Cryptobase.config.withCredentials;
-export const incrementalOrderBook = () => Cryptobase.config.incrementalOrderBook;
-export const isResizableGrid = () => Cryptobase.config.isResizable;
-export const isDraggableGrid = () => Cryptobase.config.isDraggable;
-export const isFinexEnabled = () => Cryptobase.config.finex;
-export const showLanding = () => Cryptobase.config.showLanding;
-export const sentryEnabled = () => Cryptobase.config.sentryEnabled;
-export const captchaLogin = () => Cryptobase.config.captchaLogin;
+export const withCredentials = () => convertToBoolean(Cryptobase.config.withCredentials);
+export const incrementalOrderBook = () => convertToBoolean(Cryptobase.config.incrementalOrderBook);
+export const isResizableGrid = () => convertToBoolean(Cryptobase.config.isResizable);
+export const isDraggableGrid = () => convertToBoolean(Cryptobase.config.isDraggable);
+export const isFinexEnabled = () => convertToBoolean(Cryptobase.config.finex);
+export const showLanding = () => convertToBoolean(Cryptobase.config.showLanding);
+export const sentryEnabled = () => convertToBoolean(Cryptobase.config.sentryEnabled);
+export const captchaLogin = () => convertToBoolean(Cryptobase.config.captchaLogin);
 export const minutesUntilAutoLogout = () => Cryptobase.config.minutesUntilAutoLogout;
 export const sessionCheckInterval = () => Cryptobase.config.sessionCheckInterval;
 export const balancesFetchInterval = () => Cryptobase.config.balancesFetchInterval;
@@ -67,7 +71,7 @@ export const orderBookSideLimit = () => Number(Cryptobase.config.storage.orderBo
 export const passwordEntropyStep = () => Number(Cryptobase.config.passwordEntropyStep);
 export const languages = (Cryptobase.config.languages && Cryptobase.config.languages.length > 0) ? Cryptobase.config.languages : ['en'];
 export const kycSteps = () => Cryptobase.config.kycSteps;
-export const isUsernameEnabled = () => Cryptobase.config.usernameEnabled;
+export const isUsernameEnabled = () => convertToBoolean(Cryptobase.config.usernameEnabled);
 export const captchaType = () => Cryptobase.config.captcha_type;
 export const captchaId = () => Cryptobase.config.captcha_id;
-export const passwordMinEntropy = () => Cryptobase.config.password_min_entropy;
+export const passwordMinEntropy = () => Number(Cryptobase.config.password_min_entropy);
