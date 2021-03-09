d399d208519c15277a726e22d1d728684aa1b479
josadcha <josadcha@heliostech.fr>
2021-03-09 15:17:53 +0200

    Fix: send correct param refid on signup

--- web/src/screens/SignUpScreen/index.tsx
+++ web/src/screens/SignUpScreen/index.tsx
@@ -378,7 +378,7 @@ class SignUp extends React.Component<Props> {
         }
 
         if (refId) {
-            payload = { ...payload, refId };
+            payload = { ...payload, refid: refId };
         }
 
         switch (captchaType()) {
