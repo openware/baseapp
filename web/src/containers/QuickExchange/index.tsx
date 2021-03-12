de46d62762755d0df3d4bc5e910aba2fd7df2e8b
Oleksandr Khlopiachyi <akhlopiachyi@heliostech.fr>
2021-03-12 02:20:31 +0200

    Feature: Quick exchange logic

--- web/src/containers/QuickExchange/index.tsx
+++ web/src/containers/QuickExchange/index.tsx
@@ -1,9 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
-import {
-    useCurrenciesFetch,
-} from '../../hooks';
+import { Button } from 'react-bootstrap';
+import { useCurrenciesFetch } from '../../hooks';
 import {
     marketsFetch,
     selectMarkets,
@@ -13,6 +12,7 @@ import {
 } from '../../modules'
 import { SwipeIcon } from '../../assets/images/swipe';
 import { QuickExchangeForm, DropdownComponent } from '../../components';
+import { onlyUnique, getCurrencyForMarket, getCurrencyFiltred, getMarket, getWallets, getWallet } from './helpers';
 
 interface QuickExchangeInterface {
     amount: string;
@@ -24,91 +24,207 @@ const DEFAULT_VALUE: QuickExchangeInterface = {
     amount: '',
     currency: '',
     iconURL: '',
-}
+};
 
 export const QuickExchangeContainer = () => {
+    const [base, setBaseData] = useState<QuickExchangeInterface>(DEFAULT_VALUE);
+    const [quote, setQuoteData] = useState<QuickExchangeInterface>(DEFAULT_VALUE);
+    const [type, setType] = useState('buy');
+    const [dropdownKey, setDropdownKey] = useState('');
+
     const { formatMessage } = useIntl();
     const dispatch = useDispatch();
 
-    const [exchange, setExchange] = useState<QuickExchangeInterface>(DEFAULT_VALUE);
-    const [receive, setReceive] = useState<QuickExchangeInterface>(DEFAULT_VALUE)
-
     const wallets = useSelector(selectWallets) || [];
     const markets = useSelector(selectMarkets) || [];
+
+    const translate = useCallback((id: string) => formatMessage({ id: id }), [formatMessage]);
 
     useCurrenciesFetch();
 
     React.useEffect(() => {
         dispatch(walletsFetch());
-        dispatch(marketsFetch({type: 'qe'}))
+        dispatch(marketsFetch({type: 'qe'}));
     }, []);
 
-    const onlyUnique = (value, index, self) => (self.indexOf(value) === index);
+    const marketCurrency = getCurrencyForMarket(markets);
+    const marketsBaseUnit = getCurrencyFiltred(quote.currency, base.currency, dropdownKey, marketCurrency, markets);
+    const marketsQuoteUnit = getCurrencyFiltred(base.currency, quote.currency, dropdownKey, marketCurrency, markets);
+    const walletsBaseUnit = getWallets(wallets, marketsBaseUnit);
+    const walletsQuoteUnit = getWallets(wallets, marketsQuoteUnit);
+    const walletBase = getWallet(base.currency, wallets);
 
-    const getCurrencyForMarket = (markets: Market[]) => {
-        const marketBaseUnits = markets.map(item => (item.base_unit));
-        const marketQuoteUnits = markets.map(item => (item.quote_unit));
+    const walletsBaseList = walletsBaseUnit.length ? walletsBaseUnit.map(item => item.currency && item.currency.toUpperCase()) : [];
+    const walletsQuoteList = walletsQuoteUnit.length ? walletsQuoteUnit.map(item => item.currency && item.currency.toUpperCase()) : [];
 
-        const marketCurrencies =  marketBaseUnits.concat(...marketQuoteUnits)
-            .filter(onlyUnique);
-        return marketCurrencies;
-    }
-
-    const handleUpdateField= (field: string, formSide: string) => (value: string | number) => {
-        const side = formSide === 'exchange' ? {...exchange} : {...receive};
+    const handleChangeInput = (formSide: string) => (value: string) => {
+        const side = formSide === 'exchange' ? base : quote;
         const updatedFields = {
             ...side,
-            [field]: field === 'currency' ? walletsList[value] : value,
+            amount: value,
+        };
+
+        formSide === 'exchange' ? setBaseData(updatedFields) : setQuoteData(updatedFields);
+    };
+
+    const handleChangeDropdownBase = (index: number) => {
+        const market = { id: '' };
+        const value = walletsBaseList[index];
+        const marketExists = markets.some(item => item.id === (value + quote.currency).toLowerCase());
+        const marketMirrorExists = markets.some(item => item.id === (quote.currency + value).toLowerCase());
+
+        if (marketExists) {
+            market.id = (value + quote.currency).toLowerCase();
+            const currentMarket = getMarket(market.id, markets);
+
+            if (currentMarket) {
+                setType('buy');
+            }
+        } else if (marketMirrorExists) {
+            market.id = (quote.currency + value).toLowerCase();
+            const currentMarket = getMarket(market.id, markets);
+
+            if (currentMarket) {
+                setType('sell');
+            }
         }
 
-        formSide === 'exchange' ? setExchange(updatedFields) : setReceive(updatedFields);
-    }
+        if (base.currency !== value && value !== '') {
+            const newBaseCurrency: QuickExchangeInterface = {
+                amount: '',
+                currency: value,
+                iconURL: '',
+            };
+
+            setQuoteData(DEFAULT_VALUE);
+            setBaseData(newBaseCurrency);
+        }
+
+        setDropdownKey('base_unit');
+    };
+
+    const handleChangeDropdownQuote = (index: number) => {
+        const market = { id: '' };
+        const value = walletsQuoteList[index];
+        const marketExists = markets.some(item => item.id === (base.currency + value).toLowerCase());
+        const marketMirrorExists = markets.some(item => item.id === (value + base.currency).toLowerCase());
+
+        if (marketExists) {
+            market.id = (base.currency + value).toLowerCase();
+            const currentMarket = getMarket(market.id, markets);
+
+            if (currentMarket) {
+                setType('buy');
+            }
+        } else if (marketMirrorExists) {
+            market.id = (value + base.currency).toLowerCase();
+            const currentMarket = getMarket(market.id, markets);
+
+            if (currentMarket) {
+                setType('sell');
+            }
+        }
+
+        if (quote.currency !== value && quote.currency !== '') {
+            const newQuoteCurrency: QuickExchangeInterface = {
+                amount: '',
+                currency: value,
+                iconURL: '',
+            };
+            setBaseData(DEFAULT_VALUE);
+            setQuoteData(newQuoteCurrency);
+        }
+
+        setDropdownKey('quote_unit');
+    };
+
 
     const swipeFields = () => {
-        const newReceive = {...receive};
-        const newExchange = {...exchange};
+        setType(type === 'buy' ? 'sell' : 'buy');
+        setBaseData(quote);
+        setQuoteData(base);
+    };
 
-        setExchange(newReceive);
-        setReceive(newExchange);
-    }
 
-    const translate = useCallback((id: string) => formatMessage({ id: id }), [formatMessage]);
-
-    const walletsList = wallets.length ? wallets.map(item => item.currency && item.currency.toUpperCase()) : [];
-
-    const marketCurrencies = markets.length ? getCurrencyForMarket(markets) : [];
+    const handleSubmitExchange = () => {
+        console.log('submit');
+    };
 
     return (
         <div className="cr-quick-exchange">
-            <div className="cr-quick-exchange__header">{translate('page.body.quick.exchange.header')}</div>
-            <div>
-                <QuickExchangeForm
-                    field="exchange"
-                    handleChangeInput={handleUpdateField('amount', 'exchange')}
-                    value={exchange.amount}
-                />
-                <DropdownComponent
-                    className="pg-confirm__content-address__row__content-number-dropdown"
-                    list={walletsList}
-                    selectedValue={exchange.currency}
-                    onSelect={handleUpdateField('currency', 'exchange')}
-                    placeholder="Currency"
-                />
+            <div className="cr-quick-exchange__header">
+                {translate('page.body.quick.exchange.header')}
             </div>
-                <SwipeIcon onClick={swipeFields} />
-            <div>
-                <QuickExchangeForm
-                    field="receive"
-                    handleChangeInput={handleUpdateField('amount', 'receive')}
-                    value={receive.amount}
-                />
-                <DropdownComponent
-                    className="pg-confirm__content-address__row__content-number-dropdown"
-                    list={walletsList}
-                    selectedValue={receive.currency}
-                    onSelect={handleUpdateField('currency', 'receive')}
-                    placeholder="Currency"
-                />
+            <div className="cr-quick-exchange__body">
+                <div className="cr-quick-exchange__body-currency-block">
+                    <QuickExchangeForm
+                        field="exchange"
+                        handleChangeInput={handleChangeInput('exchange')}
+                        value={base.amount}
+                    />
+                    <DropdownComponent
+                        className="cr-quick-exchange__body-currency-block-dropdown"
+                        list={walletsBaseList}
+                        selectedValue={base.currency}
+                        onSelect={handleChangeDropdownBase}
+                        placeholder="Currency"
+                    />
+                </div>
+                <div className="cr-quick-exchange__body-swap">
+                    <SwipeIcon onClick={swipeFields} />
+                </div>
+                <div className="cr-quick-exchange__body-currency-block">
+                    <QuickExchangeForm
+                        field="receive"
+                        handleChangeInput={handleChangeInput('receive')}
+                        value={quote.amount}
+                    />
+                    <DropdownComponent
+                        className="cr-quick-exchange__body-currency-block-dropdown"
+                        list={walletsQuoteList}
+                        selectedValue={quote.currency}
+                        onSelect={handleChangeDropdownQuote}
+                        placeholder="Currency"
+                    />
+                </div>
+                <div className="cr-quick-exchange__body-summary">
+                    <div className="cr-quick-exchange__body-summary-currency">
+                        <div className="cr-quick-exchange__body-summary-currency-label">
+                            You exchange
+                        </div>
+                        <div className="cr-quick-exchange__body-summary-currency-value">
+                            {base.amount} {base.currency.toUpperCase()}
+                        </div>
+                    </div>
+                    <div className="cr-quick-exchange__body-summary-icons">
+                        Icons
+                    </div>
+                    <div className="cr-quick-exchange__body-summary-currency">
+                        <div className="cr-quick-exchange__body-summary-currency-label">
+                            You receive
+                        </div>
+                        <div className="cr-quick-exchange__body-summary-currency-value">
+                            {quote.amount} {quote.currency.toUpperCase()}
+                        </div>
+                    </div>
+                </div>
+                <div className="cr-quick-exchange__body-info">
+                    <div className="cr-quick-exchange__body-info-price">
+                        Estimated price: <span>~0.04147820 {base.currency.toUpperCase()}</span>
+                    </div>
+                    <div className="cr-quick-exchange__body-info-warning">
+                        Slippage is higher than 1%. Enter a lower amount to get a better price
+                    </div>
+                </div>
+                <div className="cr-quick-exchange__body-button">
+                    <Button
+                        onClick={handleSubmitExchange}
+                        size="lg"
+                        variant="primary"
+                    >
+                        Exchange
+                    </Button>
+                </div>
             </div>
         </div>
     );
