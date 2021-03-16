48e2f3bd39a6d162ca4951c6ce87783f4246080d
Alex Khlopiachyi <akhlopiachyi@heliostech.fr>
2021-03-16 19:11:26 +0200

    Enhancement: Improve regex for platform url (#510)
    * Fix: remove console error
    * Enhancement: Improve regex for platform url

--- web/src/helpers/emailValidation.ts
+++ web/src/helpers/emailValidation.ts
@@ -6,5 +6,5 @@ export const ERROR_INVALID_EMAIL = 'page.header.signUp.email.message.error';
 export const ERROR_PASSWORD_CONFIRMATION = 'page.header.signUp.confirmPassword.message.error';
 export const ERROR_EMPTY_PASSWORD = 'page.header.signIn.password.message.error';
 
-export const DOMAIN_REGEX = /^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9\-\.]){1,61}(?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3}))(?:\:[0-9]{1,5})?$/;
+export const DOMAIN_REGEX = /^(?:(?:(?:[a-zA-z\-]+)\:\/\/{1,3})(?:[a-zA-Z0-9])(?:[a-zA-Z0-9\-\.]){1,61}(?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3}))(?:\:[0-9]{1,5})?$/;
 export const EXCHANGE_NAME_REGEX = /^[0-9A-Za-z]*$/;
