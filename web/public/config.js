c955f4ab93d2fef2fdefef7e3bd60b083fc339d1
Andrew Kushniruk <akushniruk@openware.com>
2021-04-08 11:46:14 +0300

    Fix: fetchConfigs has already been declared (#531)

--- web/public/config.js
+++ web/public/config.js
@@ -1,9 +1,13 @@
-const fetchConfig = new XMLHttpRequest();
-const hostUrl = window.location.hostname === 'localhost' ? 'http://localhost:9002' : window.location.origin;
-
-fetchConfig.open('GET', `${hostUrl}/api/v2/sonic/public/config`, false);
-fetchConfig.send(null);
-
-if (fetchConfig.status === 200) {
-    window.env = JSON.parse(fetchConfig.responseText);
+function fetchConfigs() {
+    const fetchConfig = new XMLHttpRequest();
+    const hostUrl = window.location.hostname === 'localhost' ? 'http://localhost:9002' : window.location.origin;
+    
+    fetchConfig.open('GET', `${hostUrl}/api/v2/sonic/public/config`, false);
+    fetchConfig.send(null);
+  
+    if (fetchConfig.status === 200) {
+        window.env = JSON.parse(fetchConfig.responseText);
+    }
 }
+  
+fetchConfigs();
