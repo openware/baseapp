3bb5caa9693ebf991de5e315e05f6155e590f55d
akushniruk <akushniruk@openware.com>
2021-04-09 15:23:51 +0300

    Fix: remove config.js from build ver.

--- web/webpack/development.ts
+++ web/webpack/development.ts
@@ -1,15 +1,25 @@
 import { HotModuleReplacementPlugin } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import merge from 'webpack-merge';
 import 'webpack-dev-server';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 import commonConfig from './common';
+const rootDir = path.resolve(__dirname, '..');
 
 const config = merge(commonConfig, {
     devtool: 'cheap-module-eval-source-map',
-    plugins: [new HotModuleReplacementPlugin(), new ForkTsCheckerWebpackPlugin({})],
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin({}),
+        new HtmlWebpackPlugin({
+            template: path.resolve(rootDir, 'src/app/template_dev.html'),
+            hash: true,
+            chunks: ['common', 'bundle', 'styles'],
+        }),
+    ],
     module: {
         rules: [
             {
