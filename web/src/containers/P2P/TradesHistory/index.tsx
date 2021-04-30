58950f6117fe482b55538b38ee1a37698d8200ab
Dhanuwat Panyakom <dump@yellow.com>
2021-04-30 17:33:54 +0700

    Fix: p2p trade history counter party value (#597)

--- web/src/containers/P2P/TradesHistory/index.tsx
+++ web/src/containers/P2P/TradesHistory/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useCallback } from 'react';
+import React, { FC, ReactElement, useCallback, useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import { Spinner } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
@@ -27,7 +27,7 @@ import {
 import { isUsernameEnabled } from 'src/api';
 
 const TradesHistory: FC = (): ReactElement => {
-    const intl = useIntl();
+    const { formatMessage } = useIntl();
     const history = useHistory();
     const dispatch = useDispatch();
 
@@ -40,6 +40,8 @@ const TradesHistory: FC = (): ReactElement => {
     const nextPageExists = useSelector((state: RootState) => selectP2PTradesHistoryNextPageExists(state, DEFAULT_TABLE_PAGE_LIMIT));
     const currencies = useSelector(selectCurrencies);
 
+    const translate = useCallback((id: string) => formatMessage({ id }), []);
+
     const onClickPrevPage = useCallback(() => {
         dispatch(p2pTradesHistoryFetch({ page: Number(page) - 1, limit: DEFAULT_TABLE_PAGE_LIMIT }));
     }, [p2pTradesHistoryFetch, page, DEFAULT_TABLE_PAGE_LIMIT]);
@@ -48,29 +50,56 @@ const TradesHistory: FC = (): ReactElement => {
         dispatch(p2pTradesHistoryFetch({ page: Number(page) + 1, limit: DEFAULT_TABLE_PAGE_LIMIT }));
     }, [p2pTradesHistoryFetch, page, DEFAULT_TABLE_PAGE_LIMIT]);
 
-    const renderHeaders = () => {
+    const renderHeaders = useMemo(() => {
         return [
-            intl.formatMessage({id: 'page.body.p2p.order_history.date'}),
-            intl.formatMessage({id: 'page.body.p2p.order_history.side'}),
-            intl.formatMessage({id: 'page.body.p2p.order_history.price'}),
-            intl.formatMessage({id: 'page.body.p2p.order_history.quantity'}),
-            intl.formatMessage({id: 'page.body.p2p.order_history.counterparty'}),
-            intl.formatMessage({id: 'page.body.p2p.order_history.status'}),
+            translate('page.body.p2p.order_history.date'),
+            translate('page.body.p2p.order_history.side'),
+            translate('page.body.p2p.order_history.price'),
+            translate('page.body.p2p.order_history.quantity'),
+            translate('page.body.p2p.order_history.counterparty'),
+            translate('page.body.p2p.order_history.status'),
         ];
-    };
+    }, []);
 
-    const renderContent = () => {
+    const renderTableRow = useCallback((item: P2POrder) => {
+        const { created_at, side, amount, state, user_uid } = item;
+        const { price, quote, base, user, uid } = item.offer;
+        const sideColored = setTradesType(side);
+        const stateColored = setStateType(state);
+        const pricePrecision = currencies.find(item => item.id === quote)?.precision;
+        const amountPrecision = currencies.find(item => item.id === base)?.precision;
+
         return [
+            localeDate(created_at, 'fullDate'),
+            <span style={{ color: sideColored.color }}>{sideColored.text}</span>,
+            `${Decimal.format(price, pricePrecision || DEFAULT_FIAT_PRECISION, ',')} ${base.toUpperCase()}/${quote.toUpperCase()}`,
+            `${Decimal.format(amount, amountPrecision || DEFAULT_CCY_PRECISION, ',')} ${base.toUpperCase()}`,
+            <span>{user?.user_nickname || uid}</span>,
+            <span style={{ color: stateColored.color }}>{stateColored.text}</span>,
+        ];
+    }, [currencies]);
+
+    const retrieveData = useMemo(() => {
+        return list.map(item => renderTableRow(item));
+    }, [list, renderTableRow]);
+
+    const onRowClick = useCallback((index: string) => {
+        const orderId = list[index]?.id;
+        orderId && history.push(`/p2p/order/${orderId}`);
+    }, [list]);
+
+    const renderContent = useMemo(() => {
+        return (
             <div className="pg-p2p-trades-history">
-                <h1>{intl.formatMessage({id: 'page.body.p2p.order_history.title'})}</h1>
+                <h1>{translate('page.body.p2p.order_history.title')}</h1>
                 <span className="pg-p2p-trades-history-back" onClick={() => history.push('/p2p')}>
                     <div className="pg-p2p-trades-history-back-arrow">
                         <LeftArrowIcon className="pg-p2p-trades-history-back-arrow-image"/>
                     </div>
-                    {intl.formatMessage({id: 'page.body.p2p.order_history.back'})}
+                    {translate('page.body.p2p.order_history.back')}
                 </span>
                 <div className="cr-p2p-history-table">
-                    <History headers={renderHeaders()} data={retrieveData()} onSelect={onRowClick}/>
+                    <History headers={renderHeaders} data={retrieveData} onSelect={onRowClick}/>
                     {list.length > 0 &&
                     <Pagination
                         firstElemIndex={firstElemIndex}
@@ -83,45 +112,30 @@ const TradesHistory: FC = (): ReactElement => {
                     />}
                 </div>
             </div>
-        ];
-    };
-
-    const retrieveData = () => {
-        return [...list].map(item => renderTableRow(item));
-    };
-
-    const onRowClick = useCallback((index: string) => {
-        const orderId = list[index]?.id;
-        orderId && history.push(`/p2p/order/${orderId}`);
-    }, [history, list]);
-
-    const renderTableRow = (item: P2POrder) => {
-        const { created_at, side, amount, state, user_uid } = item;
-        const { price, quote, base, user } = item.offer;
-        const sideColored = setTradesType(side);
-        const stateColored = setStateType(state);
-        const pricePrecision = currencies.find(item => item.id === quote)?.precision;
-        const amountPrecision = currencies.find(item => item.id === base)?.precision;
-
-        return [
-            localeDate(created_at, 'fullDate'),
-            <span style={{ color: sideColored.color }}>{sideColored.text}</span>,
-            `${Decimal.format(price, pricePrecision || DEFAULT_FIAT_PRECISION, ',')} ${base.toUpperCase()}/${quote.toUpperCase()}`,
-            `${Decimal.format(amount, amountPrecision || DEFAULT_CCY_PRECISION, ',')} ${base.toUpperCase()}`,
-            <span>{user?.user_nickname || user_uid}</span>,
-            <span style={{ color: stateColored.color }}>{stateColored.text}</span>,
-        ];
-    };
+        );
+    }, [list, firstElemIndex, lastElemIndex, total, page, nextPageExists]);
 
     React.useEffect(() => {
         dispatch(p2pTradesHistoryFetch({ limit: DEFAULT_TABLE_PAGE_LIMIT, page }));
     }, [dispatch]);
 
+    const loader = useMemo(() => {
+        return fetching && <div className="text-center"><Spinner animation="border" variant="primary" /></div>;
+    }, [fetching]);
+
+    const content = useMemo(() => {
+        return list.length ? renderContent : null;
+    }, [list]);
+
+    const noData = useMemo(() => {
+        return !list.length && !fetching ? <p className="pg-history-elem__empty">{translate('page.noDataToShow')}</p> : null;
+    }, [list, fetching]);
+
     return (
         <div>
-            {fetching && <div className="text-center"><Spinner animation="border" variant="primary" /></div>}
-            {list.length ? renderContent() : null}
-            {!list.length && !fetching ? <p className="pg-history-elem__empty">{intl.formatMessage({id: 'page.noDataToShow'})}</p> : null}
+            {loader}
+            {content}
+            {noData}
         </div>
     );
 };
