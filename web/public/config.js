2e6a5ddb12c32dd32b357d886f5da60e23f97c0d
akushniruk <akushniruk@openware.com>
2021-05-04 14:59:43 +0300

    Fix: handle magic link in configs

--- web/public/config.js
+++ web/public/config.js
@@ -7,12 +7,27 @@ function fetchConfigs() {
 
     const UnavailableServiceErrors = new Set([472, 503, 513]);
 
-    if (fetchConfig.status === 200) {
-        window.env = JSON.parse(fetchConfig.responseText);
-    } else if (fetchConfig.status === 471 && window.location.pathname !== '/restriction') {
-        window.location.replace(`${window.location.origin}/restriction`);
-    } else if (UnavailableServiceErrors.has(fetchConfig.status) && window.location.pathname !== '/maintenance') {
-        window.location.replace(`${window.location.origin}/maintenance`);
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    if (window.location.pathname === '/magic-link') {
+        if (token) {
+            const sendAccessToken = new XMLHttpRequest();
+
+            sendAccessToken.open('POST', `${hostUrl}/api/v2/barong/identity/users/access`, false)
+            sendAccessToken.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+            sendAccessToken.send(JSON.stringify({ whitelink_token: token }));
+        } else {
+            window.location.replace(window.location.origin);
+        }
+    } else {
+        if (fetchConfig.status === 200) {
+            window.env = JSON.parse(fetchConfig.responseText);
+        } else if (fetchConfig.status === 471 && window.location.pathname !== '/restriction') {
+            window.location.replace(`${window.location.origin}/restriction`);
+        } else if (UnavailableServiceErrors.has(fetchConfig.status) && window.location.pathname !== '/maintenance') {
+            window.location.replace(`${window.location.origin}/maintenance`);
+        }
     }
 }
   
