a09f1a490835322ed8ffcfc239510613694fa9a7
josadcha <39953699+josadcha@users.noreply.github.com>
2021-05-17 16:35:39 +0300

    Fix: refactor EstimatedValue to prevent markets state overwritting (#645)

--- web/src/containers/Wallets/EstimatedValue/index.tsx
+++ web/src/containers/Wallets/EstimatedValue/index.tsx
@@ -1,137 +1,39 @@
 import * as React from 'react';
-import { injectIntl } from 'react-intl';
-import { connect, MapDispatchToPropsFunction } from 'react-redux';
-import { IntlProps } from '../../../';
+import { useIntl } from 'react-intl';
+import { useSelector } from 'react-redux';
+import { useMarketsFetch, useMarketsTickersFetch, useRangerConnectFetch, useWalletsFetch } from 'src/hooks';
 import { formatWithSeparators } from '../../../components';
 import { VALUATION_PRIMARY_CURRENCY, VALUATION_SECONDARY_CURRENCY } from '../../../constants';
 import { estimateUnitValue, estimateValue } from '../../../helpers/estimateValue';
 import {
-    currenciesFetch,
-    Currency,
-    marketsFetch,
-    marketsTickersFetch,
-    RootState,
     selectCurrencies,
     selectMarkets,
     selectMarketTickers,
-    selectUserLoggedIn,
     Wallet,
 } from '../../../modules';
-import { Market, Ticker } from '../../../modules/public/markets';
-import { rangerConnectFetch, RangerConnectFetch } from '../../../modules/public/ranger';
-import { RangerState } from '../../../modules/public/ranger/reducer';
-import { selectRanger } from '../../../modules/public/ranger/selectors';
 
 interface EstimatedValueProps {
     wallets: Wallet[];
 }
 
-interface ReduxProps {
-    currencies: Currency[];
-    markets: Market[];
-    tickers: {
-        [key: string]: Ticker,
-    };
-    rangerState: RangerState;
-    userLoggedIn: boolean;
-}
+type Props = EstimatedValueProps;
 
-interface DispatchProps {
-    fetchCurrencies: typeof currenciesFetch;
-    fetchMarkets: typeof marketsFetch;
-    fetchTickers: typeof marketsTickersFetch;
-    rangerConnect: typeof rangerConnectFetch;
-}
+const EstimatedValue: React.FC<Props> = (props: Props): React.ReactElement => {
+    const { formatMessage } = useIntl();
+    const translate = React.useCallback((id: string, value?: any) => formatMessage({ id: id }, { ...value }), [formatMessage]);
 
-type Props = DispatchProps & ReduxProps & EstimatedValueProps & IntlProps;
+    const { wallets } = props;
+    const currencies = useSelector(selectCurrencies);
+    const markets = useSelector(selectMarkets);
+    const tickers = useSelector(selectMarketTickers);
 
-class EstimatedValueContainer extends React.Component<Props> {
-    public componentDidMount(): void {
-        const {
-            currencies,
-            fetchCurrencies,
-            fetchMarkets,
-            fetchTickers,
-            markets,
-            rangerState: {connected},
-            tickers,
-            userLoggedIn,
-        } = this.props;
+    useMarketsTickersFetch();
+    useMarketsFetch();
+    useWalletsFetch();
+    useRangerConnectFetch();
 
-        if (!markets.length) {
-            fetchMarkets();
-        }
-
-        if (!tickers.length) {
-            fetchTickers();
-        }
-
-        if (!currencies.length) {
-            fetchCurrencies();
-        }
-
-        if (!connected) {
-            this.props.rangerConnect({withAuth: userLoggedIn});
-        }
-    }
-
-    public componentWillReceiveProps(next: Props) {
-        const {
-            currencies,
-            fetchCurrencies,
-            fetchMarkets,
-            fetchTickers,
-            markets,
-            tickers,
-        } = this.props;
-
-        if (!markets.length && next.markets.length) {
-            fetchMarkets();
-        }
-
-        if (!tickers.length && next.tickers.length) {
-            fetchTickers();
-        }
-
-        if (!currencies.length && next.currencies.length) {
-            fetchCurrencies();
-        }
-    }
-
-    public render(): React.ReactNode {
-        const {
-            currencies,
-            markets,
-            tickers,
-            wallets,
-        } = this.props;
-        const estimatedValue = estimateValue(VALUATION_PRIMARY_CURRENCY, currencies, wallets, markets, tickers);
-
-        return (
-            <div className="pg-estimated-value">
-                <div className="pg-estimated-value__container">
-                    {this.translate('page.body.wallets.estimated_value')}
-                    <span className="value-container">
-                        <span className="value">
-                            {formatWithSeparators(estimatedValue, ',')}
-                        </span>
-                        <span className="value-sign">{VALUATION_PRIMARY_CURRENCY.toUpperCase()}</span>
-                    </span>
-                    {VALUATION_SECONDARY_CURRENCY && this.renderSecondaryCurrencyValuation(estimatedValue)}
-                </div>
-            </div>
-        );
-    }
-
-    public translate = (key: string) => this.props.intl.formatMessage({id: key});
-
-    private renderSecondaryCurrencyValuation = (estimatedValue: string) => {
-        const {
-            currencies,
-            markets,
-            tickers,
-        } = this.props;
-        const estimatedValueSecondary = estimateUnitValue(VALUATION_SECONDARY_CURRENCY, VALUATION_PRIMARY_CURRENCY, +estimatedValue, currencies, markets, tickers);
+    const renderSecondaryCurrencyValuation = React.useCallback((value: string) => {
+        const estimatedValueSecondary = estimateUnitValue(VALUATION_SECONDARY_CURRENCY, VALUATION_PRIMARY_CURRENCY, +value, currencies, markets, tickers);
 
         return (
             <span className="value-container">
@@ -141,23 +43,26 @@ class EstimatedValueContainer extends React.Component<Props> {
                 <span className="value-sign">{VALUATION_SECONDARY_CURRENCY.toUpperCase()}</span>
             </span>
         );
-    };
+    }, [currencies, markets, tickers]);
+
+    const estimatedValue = React.useMemo(() => {
+        return estimateValue(VALUATION_PRIMARY_CURRENCY, currencies, wallets, markets, tickers);
+    }, [currencies, wallets, markets, tickers]);
+
+    return (
+        <div className="pg-estimated-value">
+            <span className="pg-estimated-value__title">{translate('page.body.wallets.estimated_value')}</span>
+            <div className="pg-estimated-value__container">
+                <span className="value-container">
+                    <span className="value">
+                        {formatWithSeparators(estimatedValue, ',')}
+                    </span>
+                    <span className="value-sign">{VALUATION_PRIMARY_CURRENCY.toUpperCase()}</span>
+                </span>
+                {VALUATION_SECONDARY_CURRENCY && renderSecondaryCurrencyValuation(estimatedValue)}
+            </div>
+        </div>
+    );
 }
 
-const mapStateToProps = (state: RootState): ReduxProps => ({
-    currencies: selectCurrencies(state),
-    markets: selectMarkets(state),
-    tickers: selectMarketTickers(state),
-    rangerState: selectRanger(state),
-    userLoggedIn: selectUserLoggedIn(state),
-});
-
-const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> = dispatch => ({
-    fetchCurrencies: () => dispatch(currenciesFetch()),
-    fetchMarkets: () => dispatch(marketsFetch()),
-    fetchTickers: () => dispatch(marketsTickersFetch()),
-    rangerConnect: (payload: RangerConnectFetch['payload']) => dispatch(rangerConnectFetch(payload)),
-});
-
-// tslint:disable-next-line:no-any
-export const EstimatedValue = injectIntl(connect(mapStateToProps, mapDispatchToProps)(EstimatedValueContainer)) as any;
+export { EstimatedValue };
