2137a27812214920ca8ec38c8a95ff4d0d17e91a
Sean Lee <5255084-seanlee1@users.noreply.gitlab.com>
2021-05-24 18:09:58 +0700

    Fix: unit test fix wip

--- web/src/constants/constants.test.ts
+++ web/src/constants/constants.test.ts
@@ -11,6 +11,17 @@ describe('Constants', () => {
         ['page.header.navbar.internal.transfer', '/internal-transfer', 'internal_transfer'],
     ];
 
+    const expectedRoutesForLoggedInUser2 = [
+        ['page.header.navbar.trade', '/trading/', 'trade'],
+        ['page.header.navbar.quick.exchange', '/quick-exchange', 'quick_exchange'],
+        ['page.header.navbar.wallets', '/wallets', 'wallets'],
+        ['page.header.navbar.openOrders', '/orders', 'orders'],
+        ['page.header.navbar.history', '/history', 'history'],
+        ['page.header.navbar.p2p', '/p2p', 'p2p'],
+        ['page.header.navbar.api', '/docs', 'api'],
+        ['page.header.navbar.internal.transfer', '/internal-transfer', 'internal_transfer'],
+    ];
+
     const expectedRoutesForNotLoggedInUser = [
         ['page.header.navbar.signIn', '/signin', 'signin'],
         ['page.header.signUp', '/signup', 'signup'],
@@ -21,11 +32,19 @@ describe('Constants', () => {
         expect(PG_TITLE_PREFIX).toBe('Cryptobase');
     });
 
-    it('Rendering correct correct routes if user is not logged in', () => {
-        expect(pgRoutes(false, true)).toEqual(expectedRoutesForNotLoggedInUser);
+    it('Rendering correct routes if user is not logged in', () => {
+        expect(pgRoutes(false, {})).toEqual(expectedRoutesForNotLoggedInUser);
     });
 
-    it('Rendering correct correct routes if user is not logged in', () => {
-        expect(pgRoutes(true, false)).toEqual(expectedRoutesForLoggedInUser);
+    it('Rendering correct routes if user is logged in', () => {
+        expect(pgRoutes(true, {read: ['QuickExchange']})).toEqual(expectedRoutesForLoggedInUser);
+    });
+
+    it('Rendering correct routes if user is logged in', () => {
+        expect(pgRoutes(true, {read: ['QuickExchange']})).toEqual(expectedRoutesForLoggedInUser);
+    });
+
+    it('Rendering correct routes if user is logged in with P2P ability', () => {
+        expect(pgRoutes(true, {read: ['QuickExchange', 'P2P']})).toEqual(expectedRoutesForLoggedInUser2);
     });
 });
