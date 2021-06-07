720ae63a7bf4a724ea8908a89e3f94d6ad3803fc
Andrii Kushniruk <akushniruk@openware.com>
2021-06-07 16:51:16 +0300

    Feature: add module fee group for member

---
+++ web/src/modules/user/feeGroup/sagas/feeGroupSaga.ts
@@ -0,0 +1,24 @@
+import { call, put } from 'redux-saga/effects';
+import { alertPush, sendError } from '../../..';
+import { API, RequestOptions } from '../../../../api';
+import { feeGroupData, feeGroupError, FeeGroupFetch } from '../actions';
+
+const FeeGroupOption: RequestOptions = {
+    apiVersion: 'peatio',
+};
+
+export function* feeGroupSaga(action: FeeGroupFetch) {
+    try {
+        const feeGroup = yield call(API.get(FeeGroupOption), '/account/members/me');
+        yield put(feeGroupData(feeGroup));
+        yield put(alertPush({message: ['success.fee.group.action'], type: 'success'}));
+    } catch (error) {
+        yield put(sendError({
+            error,
+            processingType: 'alert',
+            extraOptions: {
+                actionError: feeGroupError,
+            },
+        }));
+    }
+}
