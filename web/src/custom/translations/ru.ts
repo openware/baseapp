f67889da4c715b6cc95bd2940f0ae298e1c00013
josadcha <39953699+josadcha@users.noreply.github.com>
2021-07-08 13:51:13 +0300

    Fix: errored deposit state added to translations (#725)

--- web/src/custom/translations/ru.ts
+++ web/src/custom/translations/ru.ts
@@ -363,6 +363,7 @@ export const ru: LangType = {
     'page.body.history.deposit.content.status.rejected': 'Отвергнут',
     'page.body.history.deposit.content.status.skipped': 'Пропущен',
     'page.body.history.deposit.content.status.processing': 'Обрабатывается',
+    'page.body.history.deposit.content.status.errored': 'Ошибка',
 
     'page.body.history.withdraw': 'История выводов',
     'page.body.history.withdraw.header.id': 'ID',
