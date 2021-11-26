3f202ed5d0b4a6047461d94e03a1b05ffdefaa26
Dhanuwat Panyakom <dump@yellow.com>
2021-11-26 18:54:50 +0700

    fix: start timestamp candle stick period (#839)

--- web/src/helpers/getStartTimestampPeriod.ts
+++ web/src/helpers/getStartTimestampPeriod.ts
@@ -1,10 +1,10 @@
 import { tvDefaultCandles } from "src/api/config";
 
 export const getStartTimestampPeriod = (tsTo: number | string, period: number | string): number => {
-    const periodMinutes = period === '1D' ? (60 * 24)
-        : period === 'D' ? (60 * 24 * 3)
-        : period;
+    const periodSeconds = period === '1D' ? (60 * 60 * 24)
+        : period === 'D' ? (60 * 60 * 24 * 3)
+        : +period * 60;
 
-        const tsFrom = +tsTo - (+periodMinutes * tvDefaultCandles())
+        const tsFrom = +tsTo - (+periodSeconds * tvDefaultCandles())
     return tsFrom;
 }
